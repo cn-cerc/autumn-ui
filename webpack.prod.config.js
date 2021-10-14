f8edb7d2963dad206b553b31fff6b43d6ecbc30b
韦善贵 <873261248@qq.com>
2021-10-14 10:01:21 +0800

    Update webpack.prod.config.js

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,7 +1,10 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
 module.exports = {
-	entry: './src/index.js',
+	entry: './src/index.ts',
 	output: {
 		filename: './summer-ci.js'
 	},
@@ -12,12 +15,28 @@ module.exports = {
 		},
 		port: 7000,
 	},
+	devtool: 'inline-source-map',
+	module: {   // new add +
+		rules: [{
+			test: /\.tsx?$/,
+			use: 'ts-loader',
+			exclude: /node_modules/
+		}]
+	},
+	resolve: { // new add +
+		extensions: ['.tsx', '.ts', '.js']
+	},
 	plugins: [
+		new CleanWebpackPlugin(),
 		new HtmlWebpackPlugin({
 			title: 'summer-ci实例',
 			// Load a custom template (lodash by default)
 			template: './public/index.html',
 			filename: './index.html',
+		}),
+		// new webpack.HashedModuleIdsPlugin(),
+		new webpack.ProvidePlugin({
+			lod: 'lodash'
 		})
 	]
 }
