69ac1a1a6f6ef87de3792e8326e00179f82b9297
韦善贵 <873261248@qq.com>
2021-10-16 17:30:16 +0800

    增加打包配置文件，正式版本不产生源码

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -15,7 +15,14 @@ module.exports = {
 		},
 		port: 7000,
 	},
-	devtool: 'inline-source-map',
+	// 	devtool 取值
+	// 1 source-map ：
+	// 产生一个单独的source-map文件，功能最完全，但会减慢打包速度
+	// 2 eval-source-map ：
+	// 使用eval打包源文件模块，直接在源文件中写入干净完整的source-map，不影响构建速度，但影响执行速度和安全，建议开发环境中使用，生产阶段不要使用
+	// 3 hidden-source-map：
+	// 不会产生单独的map文件，（与eval-source-map类似）但开发者工具就只能看到行，但无法对应到具体的列（符号），对调试不便
+	devtool: 'eval-source-map',
 	module: {   // new add +
 		rules: [{
 			test: /\.tsx?$/,
