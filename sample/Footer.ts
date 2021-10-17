5a08a2434fb6cb6f379d503d10b7a0473edddfc5
张弓 <sz9214e@qq.com>
2021-10-17 15:40:31 +0800

    增加css样式

--- sample/Footer.ts
+++ sample/Footer.ts
@@ -3,11 +3,16 @@ import TCustomComponent from "../src/ui/TCustomComponent";
 export default class Footer extends TCustomComponent {
 
     html() {
-        return (
-            `<div>
-                开源管理：${this.props.corp} copyright @${this.props.year}
-            </div>`
-        );
+        return (`
+<div role="footer">
+    开源管理：${this.props.corp} copyright @${this.props.year}
+</div>
+            `);
     }
 
+    css() {
+        return (`
+div[role=footer] {color:blue}
+        `)
+    }
 }
--- src/ui/TCustomComponent.ts
+++ src/ui/TCustomComponent.ts
@@ -5,12 +5,24 @@ export default class TCustomComponent extends TComponent {
 
     public output(html: HtmlWriter) {
         html.print(this.html().trim());
+        //设置全局css样式
+        let css = this.css().trim();
+        if (css.length > 0) {
+            let style = document.createElement('style');
+            let node = document.createTextNode(css);
+            style.appendChild(node);
+            document.head.appendChild(style);
+        }
     }
 
     html(): string {
         return (
             `<div>not create</div>`
         )
+    }
+
+    css(): string {
+        return ('')
     }
 }
 
