63fd2c06e872e83d0463b99357925d6256412b2c
张弓 <sz9214e@qq.com>
2021-10-29 19:43:32 +0800

    Create Footer.tsx

---
+++ sample/Footer.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+type PropsType = {
+    corpName?: string;
+    year?: string;
+}
+
+export default class Footer extends React.Component<PropsType> {
+
+    render() {
+        return (
+            <div role="footer">
+                开源管理：${this.props.corpName} copyright @${this.props.year}
+            </div>
+        )
+    }
+
+}
