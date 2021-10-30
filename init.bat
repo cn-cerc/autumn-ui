a2d5823d290aca660036f354e7cb4ac9273609ce
张弓 <sz9214e@qq.com>
2021-10-30 18:42:51 +0800

    Create init.bat

---
+++ init.bat
@@ -0,0 +1,9 @@
+call cnpm i -D webpack webpack-cli
+call cnpm i -D webpack-dev-server
+call cnpm i -D html-webpack-plugin
+call cnpm i -D clean-webpack-plugin
+call cnpm i -D typescript ts-loader
+call cnpm i -D react react-dom
+call cnpm i -D @types/react @types/react-dom @babel/preset-react
+call cnpm run dev
+pause
