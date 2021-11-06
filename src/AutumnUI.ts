e798d06b5b95f9fb67b9d58a735fd4c93903b6e5
张弓 <sz9214e@qq.com>
2021-11-06 22:22:55 +0800

    Update AutumnUI.ts

--- src/AutumnUI.ts
+++ src/AutumnUI.ts
@@ -5,6 +5,7 @@ import RemoteService from "./db/RemoteService";
 import TGrid from "./ext/TGrid";
 import TGridColumn from "./ext/TGridColumn";
 import Grid from "./rcc/Grid";
+import GridConfig from "./rcc/GridConfig";
 import TComponent from "./ui/TComponent";
 
 export default class AutumnUI {
@@ -20,5 +21,5 @@ export default class AutumnUI {
     static TGrid = TGrid;
     //rcc
     static Grid = Grid;
-    
+    static GridConfig = GridConfig;
 }
