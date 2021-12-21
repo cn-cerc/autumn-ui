cea19ede3a337c9bfaf5a6dace2693d26f3efc23
胡红昌 <2220393782@qq.com>
2021-12-21 12:26:57 +0000

    !84 ErrorPage增加样式 * Update BrandDialog.tsx * ErrorPage增加样式

--- src/diteng/BrandDialog.tsx
+++ src/diteng/BrandDialog.tsx
@@ -76,7 +76,7 @@ export default class BrandDialog extends BaseDialog<BaseDialogPropsType, BrandTy
                         return <span>{span}</span>;
                     }}></Column>
                     <Column code='opera' name='操作' width='2' textAlign='center' customText={(row: DataRow) => {
-                        return <span role='opera' onClick={this.handleClick.bind(this, row.getString("Brand_"))}>操作</span>
+                        return <span role='opera' onClick={this.handleClick.bind(this, row.getString("Brand_"))}>选择</span>
                     }}></Column>
                 </DBGrid>
             </div>
