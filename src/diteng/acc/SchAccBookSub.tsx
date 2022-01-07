796950e1eca6f37707e7c91c685255b2ea576439
陈恒星 <1040826451@qq.com>
2022-01-07 12:21:45 +0000

    !118 aui增加科目明细SUB帐册、辅助核算科目汇总表前端维护界面 * 辅助核算科目汇总表 * Merge branch 'develop' into chx-CW007 * 科目明细SUB帐册

--- src/diteng/acc/SchAccBookSub.tsx
+++ src/diteng/acc/SchAccBookSub.tsx
@@ -1,17 +1,17 @@
-import React, { MouseEventHandler } from "react";
+import { data } from "jquery";
+import React from "react";
 import DataRow from "../../db/DataRow";
+import Datetime from "../../db/Datetime";
 import SClient from "../../db/SClient";
 import Toast from "../../db/Toast";
-import Utils from "../../db/Utils";
-import DBDatePicker from "../../rcc/DBDatePicker";
+import CustomForm, { CustomFormPropsType, CustomFormStateType } from "../../diteng/CustomForm";
+import MainMenu from "../../diteng/MainMenu";
 import DBDrop from "../../rcc/DBDrop";
 import DBEdit from "../../rcc/DBEdit";
 import DBGrid, { Column, OnRowChangedEvent } from "../../rcc/DBGrid";
 import MenuItem from "../../rcc/MenuItem";
 import SearchPanel, { SearchPanelOnExecute } from "../../rcc/SearchPanel";
 import ToolPanel, { ToolItem } from "../../rcc/ToolPanel";
-import CustomForm, { CustomFormPropsType, CustomFormStateType } from "../CustomForm";
-import MainMenu from "../MainMenu";
 
 type stateType = {
     client: SClient;
@@ -19,8 +19,7 @@ type stateType = {
     message: string;
 } & Partial<CustomFormStateType>
 
-export default class FrmAccCashFlow extends CustomForm<CustomFormPropsType, stateType> {
-    private _currencyList: Map<string, string> = new Map<string, string>([['所有币别', '']]);
+export default class SchAccBookSub extends CustomForm<CustomFormPropsType, stateType> {
     private _typeList: Map<string, string> = new Map<string, string>([['所有类别', '']]);
     private _nameList: Map<string, string> = new Map();
     private _nameList2: Map<string, string> = new Map();
@@ -28,20 +27,18 @@ export default class FrmAccCashFlow extends CustomForm<CustomFormPropsType, stat
     constructor(props: CustomFormPropsType) {
         super(props);
         let client = new SClient(this.props);
-        client.setService('ABB.searchAccCashFlow');
+        client.setService('AC_Amount.searchAccBookSub');
         let dataIn = new DataRow();
-        dataIn.setValue('TBDate__from', Utils.getMonthStartDay());
-        dataIn.setValue('TBDate__to', Utils.getMonthEndDay());
         dataIn.setValue('MaxRecord_', 100);
+        dataIn.setValue('YearMonth_', new Datetime().yearMonth);
         this.state = { client, dataIn, message: '' };
-        this.getCurrencyList();
         this.getTypeList();
         this.getNameList();
-        this.getName2List();
+        this.getNameList2();
     }
 
     get pageTitle(): string {
-        return '凭证现金流量项目维护';
+        return '科目明细SUB帐册';
     }
 
     content(): JSX.Element {
@@ -60,45 +57,30 @@ export default class FrmAccCashFlow extends CustomForm<CustomFormPropsType, stat
                     </ToolItem>
                 </ToolPanel>
                 <SearchPanel dataRow={this.state.dataIn} onExecute={this.btnSearch}>
-                    <DBEdit dataField='TBNo_' dataName='凭证编号' />
-                    <DBDatePicker dataField='TBDate__from' dataName='凭证日期'></DBDatePicker>
-                    <DBDatePicker dataField='TBDate__to' dataName='至'></DBDatePicker>
-                    <DBEdit dataField='AccCode_' dataName='会计科目' />
-                    <DBEdit dataField='StatementCode_' dataName='项目代码' />
-                    <DBEdit dataField='Desp_' dataName='摘要' />
-                    <DBEdit dataField='DrAmount_' dataName='借方金额' />
-                    <DBEdit dataField='CrAmount_' dataName='贷方金额' />
-                    <DBDrop dataField='Currency_' dataName='币别' options={this._currencyList}></DBDrop>
+                    <DBEdit dataField='AccCode_' dataName='科目代码' />
+                    <DBEdit dataField='YearMonth_' dataName='会计年月' />
                     <DBDrop dataField='Type_' dataName='对象类别' options={this._typeList} onChanged={this.handleSelect.bind(this)} />
                     <DBEdit dataField='ObjCode_' dataName='对象代码'></DBEdit>
-                    <DBEdit dataField='MaxRecord_' dataName='载入笔数'></DBEdit>
+                    <DBEdit dataField='1' dataName='仅显示底阶科目(勾选)' />
+                    <DBEdit dataField='2' dataName='仅显示记账科目(勾选)' />
+                    <DBEdit dataField='3' dataName='仅显示一阶科目(勾选)' />
+                    <DBEdit dataField='MaxRecord_' dataName='载入笔数' />
                 </SearchPanel>
                 <DBGrid key={this.state.client.updateKey} dataSet={this.state.client} readOnly={false} onChanged={this.onRowChanged.bind(this)}>
-                    <Column code='CashCode_' name='项目代码' width='5' >
-                        <DBEdit dataField="CashCode_"></DBEdit>
-                    </Column>
-                    <Column code='CashCodeName_' name='项目名称' width='15' customText={(row: DataRow) => {
-                        return <span>{this._nameList2.get(row.getString('CashCode_'))}</span>
-                    }}>
-                    </Column>
-                    <Column code='TBDate_' name='凭证日期' width='10' />
-                    <Column code='TBNo_' name='凭证编号' width='10' />
-                    <Column code='It_' name='序' width='2' />
-                    <Column code='Desp_' name='摘要' width='10' />
-                    <Column code='AccCode_' name='科目代码' width='5' />
-                    <Column code='Name_' name='科目名称' width='10' />
-                    <Column code='CurrAmount_' name='外币金额' width='5' />
-                    <Column code='Desp_' name='摘要' width='10' />
-                    <Column code='Currency_' name='币别' width='3' />
-                    <Column code='ExRate_' name='汇率' width='10' />
-                    <Column code='Dr_' name='借/贷' width='3' customText={(row: DataRow) => {
-                        return row.getString('DrCr_') == 'true' ? <span>1.借</span> : <span>0.贷</span>
-                    }}>
-                    </Column>
-                    <Column code='ExRate_' name='汇率' width='3' />
-                    <Column code='DrAmount_' name='借方金额' width='3' />
-                    <Column code='CrAmount_' name='贷方金额' width='3' />
-                    <Column code='TotalC_' name='冲帐金额' width='3' />
+                    <Column code='YearMonth_' name='会计年月' width='10' />
+                    <Column code='AccCode_' name='科目代码' width='10' />
+                    <Column code='AccName_' name='科目名称' width='15' customText={(row: DataRow) => {
+                        return <span>{this._nameList2.get(row.getString('AccCode_'))}</span>
+                    }}/>
+                    <Column code='OriInit_' name='期初原币余额' width='3' />
+                    <Column code='InitAmount_' name='期初母币余额' width='3' />
+                    <Column code='OriCurDr_' name='借方原币金额' width='3' />
+                    <Column code='OriCurCr_' name='贷方原币金额' width='3' />
+                    <Column code='DrAmount_' name='母币借方金额' width='3' />
+                    <Column code='CrAmount_' name='母币贷方金额' width='3' />
+                    <Column code='Amount_' name='母币余额' width='3' />
+                    <Column code='ObjInfo_' name='辅助核算项目' width='10' />
+                    <Column code='Amount_' name='母币余额' width='3' />
                     <Column code='ObjCode0_' name='部门代码' width='5' />
                     <Column code='ObjName0_' name='部门名称' width='10' customText={(row: DataRow) => {
                         return <span>{this._nameList.get(row.getString('ObjCode0_'))}</span>
@@ -148,15 +130,6 @@ export default class FrmAccCashFlow extends CustomForm<CustomFormPropsType, stat
         this.state.dataIn.setValue(`ObjName_`, `ObjCode${this.state.dataIn.getValue('Type_')}_`);
     }
 
-    async getCurrencyList() {
-        let currencyClient = new SClient(this.props);
-        currencyClient.setService('MoneyUnit');
-        await currencyClient.open();
-        currencyClient.forEach((row: DataRow) => {
-            this._currencyList.set(row.getString('Code_'), row.getString('Code_'));
-        })
-    }
-
     async getTypeList() {
         let typeClient = new SClient(this.props);
         typeClient.setService('AC_ObjT');
@@ -168,25 +141,32 @@ export default class FrmAccCashFlow extends CustomForm<CustomFormPropsType, stat
     }
 
     async getNameList() {
-        let client = new SClient(this.props);
-        client.setService('AC_Obj');
-        await client.open();
-        client.first();
-        while (client.fetch()) {
-            this._nameList.set(client.getString('Code_'), client.getString('Name_'));
+        let Nameclient = new SClient(this.props);
+        Nameclient.setService('AC_Obj');
+        await Nameclient.open();
+        Nameclient.first();
+        while (Nameclient.fetch()) {
+            this._nameList.set(Nameclient.getString('Code_'), Nameclient.getString('Name_'));
         }
         this.btnSearch(this.state.dataIn)
     }
 
-    async getName2List() {
-        let client = new SClient(this.props);
-        client.setService('AC_CashFlow');
-        await client.open();
-        client.first();
-        while (client.fetch()) {
-            this._nameList2.set(client.getString('Code_'), client.getString('Name_'));
+    async getNameList2() {
+        let Name2client = new SClient(this.props);
+        Name2client.setService('AccSubject');
+        await Name2client.open();
+        Name2client.first();
+        while (Name2client.fetch()) {
+            this._nameList2.set(Name2client.getString('AccCode_'), Name2client.getString('Name_'));
         }
         this.btnSearch(this.state.dataIn)
+    }
+
+    accCodeFormChange() {
+        if (!this.state.dataIn.getBoolean('AccCode__to')) {
+            this.state.dataIn.setValue('AccCode__to', this.state.dataIn.getValue('AccCode__from'));
+            this.setState(this.state)
+        }
     }
 
     btnSearch: SearchPanelOnExecute = async (row: DataRow) => {
@@ -200,17 +180,6 @@ export default class FrmAccCashFlow extends CustomForm<CustomFormPropsType, stat
             Toast.success('查询成功');
         }
         this.setState(this.state);
-    }
-
-    btnSave: MouseEventHandler<HTMLButtonElement> = (sender: any) => {
-        this.state.client.save();
-        if (this.state.client.state <= 0) {
-            console.log(this.state.client.message);
-            return;
-        } else {
-            Toast.success('保存成功');
-        }
-        this.setState({ ...this.state });
     }
 
     onRowChanged: OnRowChangedEvent = (row: DataRow, field: string, oldValue: string) => {
