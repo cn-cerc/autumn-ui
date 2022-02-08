bb935ca384b1478f158e5ebf5057dcff312790fa
胡红昌 <2220393782@qq.com>
2022-02-08 13:56:02 +0800

    调整报错页面结构与对应的样式(对应状态码的图片暂未同步到服务器上面)

--- src/diteng/ErrorPage.tsx
+++ src/diteng/ErrorPage.tsx
@@ -4,7 +4,8 @@ import styles from "./ErrorPage.css";
 
 type errorTypeProps = {
     msg: string,
-    error_hint: string
+    error_hint: string,
+    statuCode: number,
 }
 
 export default class ErrorPage extends WebControl<errorTypeProps> {
@@ -35,7 +36,7 @@ export default class ErrorPage extends WebControl<errorTypeProps> {
                 <div className={styles.contanier}>
                     <div className={styles.errpage}>
                         <ul>
-                            <li><img src={`https://www.diteng.site/public/images/bulb.png`} /></li>
+                            <li><img src={`https://www.diteng.site/public/images/statusCode/${this.props.statuCode}.png`} /></li>
                             <li>
                                 <h3>非常抱歉，您此项请求服务器无法处理！</h3>
                                 <p style={{ "margin": "0px", "textAlign": "left", "textIndent": "2em" }}>请您确认您的操作是否有误，或将下述异常讯息记下来：</p>
@@ -52,21 +53,33 @@ export default class ErrorPage extends WebControl<errorTypeProps> {
             return (
                 <div className={styles.contanier}>
                     <div className={styles.errpage}>
-                        <ul>
-                            <li><img src={`https://www.diteng.site/public/images/bulb.png`} /></li>
-                            <li style={{ "marginTop": "2em" }}>
-                                <h2>{this.props.error_hint}</h2>
-                                <p style={{ "margin": "0" }}>请您确认您的操作是否有误，或将下述异常讯息记下来：</p>
-                                <p style={{ "color": "red", "width": "100%", "display": "block", "margin": "0 auto 1.5em" }} id="msg">{this.props.msg}</p>
-                                <a href="javascript:history.go(-1)">返回上页</a>
-                                <a href="/public/WebDefault">返回首页</a>
-                                <a href="TFrmLogout">重新登录</a>
-                            </li>
-                        </ul>
+                        <div style={{ 'display': 'flex', 'flexDirection': 'column' }}>
+                            <div><img src={`https://www.diteng.site/public/images/statusCode/${this.props.statuCode}.png`} /></div>
+                            <div>
+                                <h2 style={{ "color": "#40485b", 'fontSize': '1.2rem', 'fontWeight': 'bold' }}>{this.props.error_hint}</h2>
+                                <div>{this.props.msg}，<a href='/public/WebDefault'>点击这里</a>返回首页</div>
+                                <div className={styles.errorMsg}>
+                                    <p>也有可能是以下原因导致您访问失败</p>
+                                    <ul>
+                                        <li>1、没有登录或登录状态失效，<a href='TFrmLogout'>点击这里</a>重新登陆</li>
+                                        <li>2、服务器找不到请求的网页或拒绝请求，<a href="" onClick={this.handleBack.bind(this)}>点击这里</a>返回上页或关闭当前页面</li>
+                                        <li>3、其他非具体原因，<a href='TFrmContact'>点击这里</a>联系客服</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div className=""></div>
                 </div>
             )
         }
+    }
+
+    handleBack(e: any) {
+        e.preventDefault();
+        if (window.history.length > 1)
+            window.history.go(-1);
+        else
+            window.close();
     }
 }
