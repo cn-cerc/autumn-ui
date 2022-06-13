40b1f09846602d6cb958d2d1243a94c574745866
王江江 <dusu163163@163.com>
2022-06-13 18:59:01 +0800

    temp

--- fpl/src/dialog/showSiteDialog.tsx
+++ fpl/src/dialog/showSiteDialog.tsx
@@ -1,26 +1,24 @@
 import React from "react";
 import { BaseDialogPropsType, DataRow, DataSet, BaseDialogStateType, BaseDialog, SearchPanel, DBEdit, DBGrid, Column } from "autumn-ui";
 import styles from "./showSiteDialog.css";
-import { data } from "jquery";
 
 type UserTypeProps = {
+    inputVal: string
 } & Partial<BaseDialogPropsType>
 
 type UserTypeState = {
-    list: string[],
-    sheng: string,
-    shi: string,
-    xian: string,
-    area1ShowHide: boolean,
-    area2ShowHide: boolean,
-    area3ShowHide: boolean,
-    shengText: string,
+    listCity: string[],
+    listCounty: string[],
+    Area: string,
+    City: string,
+    County: string,
+    AreaText: string,
     cityText: string,
-    xianText: string,
-    shengHTML: string,
-    shi_HTML: string,
-    xian_HTML: string,
-    color: any[]
+    CountyText: string,
+    showHideIndex: number,
+    colorArea: boolean[],
+    colorCity: boolean[],
+    colorCounty: boolean[]
 } & Partial<BaseDialogStateType>
 
 
@@ -34,20 +32,18 @@ export default class showSiteDialog extends BaseDialog<UserTypeProps, UserTypeSt
             ...this.state,
             width: '45rem',
             height: '35rem',
-            sheng: null,
-            shi: null,
-            xian: null,
-            area1ShowHide: true,
-            area2ShowHide: false,
-            area3ShowHide: false,
-            shengText: '省份',
+            Area: null,
+            City: null,
+            County: null,
+            AreaText: '省份',
             cityText: '城市',
-            xianText: '县区',
-            shengHTML: '',
-            shi_HTML: '',
-            xian_HTML: '',
-            color: [],
-            list: [],
+            CountyText: '县区',
+            listCity: ['请选择'],
+            listCounty: ['请选择'],
+            showHideIndex: 0,
+            colorArea: [],
+            colorCity: [],
+            colorCounty: []
         };
     }
 
@@ -56,9 +52,34 @@ export default class showSiteDialog extends BaseDialog<UserTypeProps, UserTypeSt
     }
 
     async init() {
-        setTimeout(() => {
-            this.onchange()
-        }, 100);
+        let cookie = this.props.inputVal.split("/")
+        if (cookie.length == 3) {
+            this.getData(cookie[0], 'City')
+            this.getData(cookie[0] + "`" + cookie[1], 'County')
+            setTimeout(() => {
+                let index0 = this.provinceArr.findIndex(value => value == cookie[0])
+                let colorArea = new Array(this.provinceArr.length).fill(false)
+                colorArea[index0] = true
+                let index1 = this.state.listCity.findIndex(value => value == cookie[1])
+                let colorCity = new Array(this.state.listCity.length).fill(false)
+                colorCity[index1] = true
+                let index2 = this.state.listCounty.findIndex(value => value == cookie[2])
+                let colorCounty = new Array(this.state.listCounty.length).fill(false)
+                colorCounty[index2] = true
+                this.setState({
+                    Area:cookie[0],
+                    AreaText:cookie[0],
+                    City:cookie[1],
+                    cityText:cookie[1],
+                    County:cookie[2],
+                    CountyText:cookie[2],
+                    showHideIndex: 2,
+                    colorArea: colorArea,
+                    colorCity: colorCity,
+                    colorCounty: colorCounty,
+                })
+            }, 100);
+        }
     }
 
     content() {
@@ -71,170 +92,135 @@ export default class showSiteDialog extends BaseDialog<UserTypeProps, UserTypeSt
                     </div>
 
                     <div className={styles.select}>
-                        <li id="sheng" onClick={this.toggle.bind(this, 'Area1_')}>{this.state.shengText}</li>
-                        <li id="shi" onClick={this.toggle.bind(this, 'Area2_')}>{this.state.cityText}</li>
-                        <li id="xian" onClick={this.toggle.bind(this, 'Area3_')}>{this.state.xianText}</li>
-                    </div>
-                    <div className={`${this.state.area1ShowHide ? styles.show : styles.hide} ${styles.site}`} id='Area1_'>
-                        {this.getProvince()}
+                        <li onClick={this.toggle.bind(this, 0)}>{this.state.AreaText}&gt;&gt;</li>
+                        <li onClick={this.toggle.bind(this, 1)}>{this.state.cityText}&gt;&gt;</li>
+                        <li onClick={this.toggle.bind(this, 2)}>{this.state.CountyText}</li>
                     </div>
 
-                    <div className={`${this.state.area2ShowHide ? styles.show : styles.hide} ${styles.site}`} id='Area2_' dangerouslySetInnerHTML={{ __html: this.state.shi_HTML }}>
-                        {/* {this.getCityList()} */}
-                    </div>
-
-                    <div className={`${this.state.area3ShowHide ? styles.show : styles.hide} ${styles.site}`} id='Area3_' dangerouslySetInnerHTML={{ __html: this.state.xian_HTML }}>
-
+                    <div className={styles.site}>
+                        {this.getShowHide()}
                     </div>
                 </div>
             </div>
         )
     }
 
+    getShowHide() {
+        let dom;
+        switch (this.state.showHideIndex) {
+            case 0:
+                dom = <React.Fragment>{this.getProvince()}</React.Fragment>
+                break;
+            case 1:
+                dom = <React.Fragment>{this.getListCity()}</React.Fragment>
+                break;
+            case 2:
+                dom = <React.Fragment>{this.getListCounty()}</React.Fragment>
+                break;
+        }
+        return dom;
+    }
+
     getProvince() {
-        let list = this.provinceArr.map((province: string, index: number) => {
-            return <a key={index}>{province}</a>
+        let list = this.provinceArr.map((data: string, index: number) => {
+            return <a className={this.state.colorArea[index] ? styles.color : styles} onClick={this.Area.bind(this, index)} key={index}>{data}</a>
         })
         return list;
     }
 
-    getCityList() {
-        let list = this.provinceArr.map((province: string, index: number) => {
-            return <a key={index}>{province}</a>
+    getListCity() {
+        let list = this.state.listCity.map((data: string, index: number) => {
+            if (index == 0) {
+                return <a key={index}>{data}</a>
+            } else {
+                return <a className={this.state.colorCity[index] ? styles.color : styles} onClick={this.City.bind(this, index)} key={index}>{data}</a>
+            }
         })
         return list;
     }
 
-    toggle(dom: any) {
+    getListCounty() {
+        let list = this.state.listCounty.map((data: string, index: number) => {
+            if (index == 0) {
+                return <a key={index}>{data}</a>
+            } else {
+                return <a className={this.state.colorCounty[index] ? styles.color : styles} onClick={this.County.bind(this, index)} key={index}>{data}</a>
+            }
+        })
+        return list;
+    }
+
+    Area(index: number) {
+        let colorList = new Array(this.provinceArr.length).fill(false)
+        colorList[index] = true
+        this.state.Area = this.provinceArr[index]
+        this.getData(this.state.Area, 'City')
         this.setState({
-            area1ShowHide: false,
-            area2ShowHide: false,
-            area3ShowHide: false,
+            showHideIndex: 1,
+            colorArea: colorList
         })
-        switch (dom) {
-            case 'Area1_':
-                this.setState({
-                    area1ShowHide: true
-                })
-                break;
-            case 'Area2_':
-                this.setState({
-                    area2ShowHide: true
-                })
-                break;
-            case 'Area3_':
-                this.setState({
-                    area3ShowHide: true
-                })
-                break;
+        if (this.state.AreaText == this.provinceArr[index]) {
+            return
+        } else {
+            this.setState({
+                AreaText: this.provinceArr[index],
+                cityText: "城市",
+                CountyText: "县区",
+                City: null,
+                County: null,
+                colorCity: [],
+                colorCounty: [],
+                listCounty: []
+            })
         }
+        return false
     }
 
-    onchange() {
-        let this_ = this
-        let but = document.querySelectorAll("#Area1_>a")
-        for (let i = 0; i < but.length; i++) {
-            //@ts-ignore
-            but[i].onclick = function () {
-                this_.state.sheng = this.innerHTML
-                this_.fetch(this_.state.sheng, '#Area2_', this_)
-                this_.setState({
-                    shi_HTML: "",
-                    xian_HTML: "",
-                    area1ShowHide: false,
-                    area2ShowHide: true,
-                    area3ShowHide: false,
-                })
-                $("#Area1_>a").css('background', '');
-                console.dir(this)
-                $(this).css('background', '#3273F4')
-                this_.onchange()
-                if ($("#sheng").html() == this.innerHTML) {
-                    return
-                } else {
-                    this_.setState({
-                        shengText: this.innerHTML,
-                        cityText: "城市",
-                        xianText: "县区",
-                        shi: null,
-                        xian: null,
-                    })
-                }
-                return false
-            }
+    City(index: number) {
+        let colorList = new Array(this.state.listCity.length).fill(false)
+        colorList[index] = true
+        this.state.City = this.state.listCity[index]
+        let site = this.state.Area + "`" + this.state.City
+        this.getData(site, 'County')
+        this.setState({
+            showHideIndex: 2,
+            colorCity: colorList
+        })
+        if (this.state.cityText == this.state.listCity[index]) {
+            return
+        } else {
+            this.setState({
+                cityText: this.state.listCity[index],
+                CountyText: "县区",
+                County: null,
+                colorCounty: []
+            })
         }
-
-        setTimeout(() => {
-            let shi = document.querySelectorAll("#Area2_>a")
-            for (let k = 0; k < shi.length; k++) {
-                //@ts-ignore
-                shi[k].onclick = function () {
-                    this_.state.shi = this.innerHTML
-                    let xxx = this_.state.sheng + "`" + this_.state.shi
-                    this_.fetch(xxx, '#Area3_', this_)
-                    this_.setState({
-                        xian_HTML: "",
-                        area1ShowHide: false,
-                        area2ShowHide: false,
-                        area3ShowHide: true,
-                    })
-                    $("#Area2_>a").css('background', '')
-                    $(this).css('background', '#3273F4')
-                    this_.onchange()
-
-                    if ($("#shi").html() == this.innerHTML) {
-                        return
-                    } else {
-                        this_.setState({
-                            cityText: this.innerHTML,
-                            xianText: "县区",
-                            xian: null
-                        })
-                    }
-                    return false
-                }
-            }
-        }, 100);
-
-        setTimeout(() => {
-            let xian = document.querySelectorAll("#Area3_>a")
-            for (let k = 0; k < xian.length; k++) {
-                //@ts-ignore
-                xian[k].onclick = function () {
-                    this_.state.xian = this.innerHTML
-                    this_.setState({
-                        xianText: this.innerHTML,
-                    })
-                    $("#Area3_>a").css('background', '')
-                    $(this).css('background', '#3273F4')
-                    let x = this_.props.inputId.split(",");
-                    $("#" + x[0]).val(this_.state.sheng + "/" + this_.state.shi + "/" + this_.state.xian)
-                    $("#" + x[1]).val(this_.state.sheng + "/" + this_.state.shi + "/" + this_.state.xian)
-                    this_.handleSelect();
-                    return false
-                }
-            }
-        }, 100);
-
+        return false
     }
 
-    handleClickBySite() {
-        if (this.state.sheng == null) {
-            alert("请选择省份")
-            return
-        }
-        if (this.state.shi == null) {
-            alert("请选择城市")
-            return
-        }
-        if (this.state.xian == null) {
-            alert("请选择县区")
-            return
-        }
-
+    County(index: number) {
+        let colorList = new Array(this.state.listCounty.length).fill(false)
+        colorList[index] = true
+        this.state.County = this.state.listCounty[index]
+        this.setState({
+            CountyText: this.state.listCounty[index],
+            colorCounty: colorList
+        })
+        let inputArr = this.props.inputId.split(",");
+        $("#" + inputArr[0]).val(this.state.Area + "/" + this.state.City + "/" + this.state.County)
+        $("#" + inputArr[1]).val(this.state.Area + "/" + this.state.City + "/" + this.state.County)
+        this.handleSelect();
+        return false
     }
 
-    fetch(site: string, dom: string, this_: this) {
+    toggle(num: number) {
+        this.setState({
+            showHideIndex: num
+        })
+    }
+
+    getData(site: string, dom: string) {
         fetch('./BaseArea', {
             method: 'POST',
             headers: {
@@ -244,49 +230,36 @@ export default class showSiteDialog extends BaseDialog<UserTypeProps, UserTypeSt
         }).then((data) => {
             return data.json();
         }).then((data) => {
-            console.log(data.areaList)
-            this_.setState({
-                list: data.areaList
-            })
-            console.log(this.state.list)
-            // $.each(data.areaList, function (key, value) {
-            //     if (key == 0)
-            //         // $(dom).append(
-            //         //     "<div>" + value + "</div>");
-            //         switch (dom) {
-            //             case '#Area2_':
-            //                 this_.setState({
-            //                     shi_HTML: "<div>" + value + "</div>",
-            //                 })
-            //                 break;
-            //             case '#Area3_':
-            //                 this_.setState({
-            //                     xian_HTML: "<div>" + value + "</div>",
-            //                 })
-            //                 break;
-            //         }
-
-            //     else
-            //         // $(dom).append(
-            //         //     "<a>" + value + "</a>");
-            //         switch (dom) {
-            //             case '#Area2_':
-            //                 this_.setState({
-            //                     shi_HTML: this_.state.shi_HTML + "<a>" + value + "</a>",
-            //                 })
-            //                 break;
-            //             case '#Area3_':
-            //                 this_.setState({
-            //                     xian_HTML: this_.state.xian_HTML + <a className={''}> + value + </a>,
-            //                 })
-            //                 break;
-            //         }
-
-            // });
-
+            switch (dom) {
+                case 'City':
+                    this.setState({
+                        listCity: data.areaList
+                    })
+                    break;
+                case "County":
+                    this.setState({
+                        listCounty: data.areaList
+                    })
+                    break;
+            }
         })
     }
 
+    handleClickBySite() {
+        console.log(this.props.children)
+        if (this.state.Area == null) {
+            alert("请选择省份")
+            return
+        }
+        if (this.state.City == null) {
+            alert("请选择城市")
+            return
+        }
+        if (this.state.County == null) {
+            alert("请选择县区")
+            return
+        }
+    }
 }
 
 
