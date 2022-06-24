390ab3f2b120439f3a1b9fd73e26f8bfe1e55c67
韦善贵 <873261248@qq.com>
2022-06-24 10:04:43 +0800

    Update FrmMessage.tsx

--- diteng/src/page/FrmMessage.tsx
+++ diteng/src/page/FrmMessage.tsx
@@ -43,7 +43,7 @@ type messageDetail = {
     remarkText_: string
 };
 
-export const timing = 50;
+export const timing = 5;
 
 export default class FrmMessage extends WebControl<FrmMessageTypeProps, FrmMessageTypeState> {
     private timer: any = null;
