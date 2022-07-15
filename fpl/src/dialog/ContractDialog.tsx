4c022695ce17266cb09522d410a56a753eee46ef
封威 <1876381677@qq.com>
2022-07-15 19:50:52 +0800

    合同添加栏位

--- fpl/src/dialog/ContractDialog.tsx
+++ fpl/src/dialog/ContractDialog.tsx
@@ -43,17 +43,20 @@ export default class ContractDialog extends BaseDialog<ContractProps, StaffTypeS
         return (
             <div role="content" className={styles.main}>
                 <SearchPanel dataRow={this.state.dataIn} onExecute={this.init.bind(this)}>
-                    <DBEdit dataField="contract_no_" dataName="合同名称" autoFocus></DBEdit>
+                    <DBEdit dataField="contract_type_name_" dataName="合同名称" autoFocus></DBEdit>
                 </SearchPanel>
                 <DBGrid dataSet={this.state.dataSet} openPage={false}>
-                    <ColumnIt/>
-                    <Column code="contract_no_" name="合同名称" width="100"></Column>
-                    <Column code="party_a_name_" name="甲方公司" width="100"></Column>
-                    <Column code="party_b_name_" name="乙方公司" width="100"></Column>
+                    <ColumnIt />
+                    <Column code="contract_type_name_" name="合同名称" width="130"></Column>
+                    <Column code="party_a_name_" name="甲方公司" width="70"></Column>
+                    <Column code="party_b_name_" name="乙方公司" width="70"></Column>
+                    <Column code="cargo_unit_price_" name="订单价" width="50"></Column>
+                    <Column code="arrangecar_unit_price_" name="运单价" width="50"></Column>
+                    <Column code="cargo_loss_rate_" name="货损率" width="40"></Column>
                     <Column code="contract_amount_" name="合同金额" width="50"></Column>
-                    <Column code="recharged_amount_" name="已充值金额" width="50"></Column>
+                    <Column code="recharged_amount_" name="充值金额" width="50"></Column>
                     <Column code="remaining_amount_" name="可用余额" width="50"></Column>
-                    <Column code="opera" name="操作" width="30" textAlign='center' customText={(row: DataRow)=>{
+                    <Column code="opera" name="操作" width="50" textAlign='center' customText={(row: DataRow) => {
                         return <span role="auiOpera" id='category' onClick={this.handleClick.bind(this, row)}>选择</span>
                     }}></Column>
                 </DBGrid>
@@ -66,10 +69,16 @@ export default class ContractDialog extends BaseDialog<ContractProps, StaffTypeS
         let input1 = document.getElementById(inputIds[0]) as HTMLInputElement;
         let input2 = document.getElementById(inputIds[1]) as HTMLInputElement;
         let input3 = document.getElementById("remaining_amount_") as HTMLInputElement;
-        
+        let input4 = document.getElementById("unit_price_") as HTMLInputElement;
+        let input5 = document.getElementById("waybill_unit_price_") as HTMLInputElement;
+        let input6 = document.getElementById("cargo_loss_rate_") as HTMLInputElement;
+
         input1.value = dataRow.getString('contract_no_');
         input2.value = dataRow.getString('contract_no_');
         input3.value = dataRow.getString('remaining_amount_');
+        input4.value = dataRow.getString('cargo_unit_price_');
+        input5.value = dataRow.getString('arrangecar_unit_price_');
+        input6.value = dataRow.getString('cargo_loss_rate_');
         this.handleSelect();
     }
 }
