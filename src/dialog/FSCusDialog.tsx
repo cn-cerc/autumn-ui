c5de35998f2135b051453816525f55e396c7f4e4
胡红昌 <2220393782@qq.com>
2022-08-02 16:07:21 +0800

    完善极速版发货人开窗

--- src/dialog/FSCusDialog.tsx
+++ src/dialog/FSCusDialog.tsx
@@ -8,22 +8,26 @@ type FSCusDialogTypeState = {
     dataSet: DataSet
 } & Partial<BaseDialogStateType>
 
-export default class FSCusDialog extends BaseDialog<BaseDialogPropsType, FSCusDialogTypeState> {
-    constructor(props: BaseDialogPropsType) {
+type FSCusDialogTypeProsp = {
+    nameId: string,
+    phoneId: string
+} & Partial<BaseDialogPropsType>
+
+export default class FSCusDialog extends BaseDialog<FSCusDialogTypeProsp, FSCusDialogTypeState> {
+    constructor(props: FSCusDialogTypeProsp) {
         super(props);
         let dataIn = new DataRow();
         dataIn.setValue('MaxRecord_', 100);
         this.state = {
             ...this.state,
-            width: '40rem',
+            width: '45rem',
             height: '30rem',
             dataIn,
             dataSet: new DataSet()
         }
     }
 
-    componentDidMount(): void {
-        super.componentDidMount();
+    componentWillMount() {
         this.init();
     }
 
@@ -44,7 +48,7 @@ export default class FSCusDialog extends BaseDialog<BaseDialogPropsType, FSCusDi
     }
 
     content(): JSX.Element {
-        return <div className={styles.main}>
+        return <div role='content' className={styles.main}>
             <SearchPanel dataRow={this.state.dataIn} onExecute={this.init.bind(this)}>
                 <DBEdit dataRow={this.state.dataIn} dataField='SearchText_' dataName='查询条件' placeholder='请输入查询条件' autoFocus />
                 <DBEdit dataRow={this.state.dataIn} dataField='MaxRecord_' dataName='载入笔数' placeholder='请输载入笔数' />
@@ -57,22 +61,22 @@ export default class FSCusDialog extends BaseDialog<BaseDialogPropsType, FSCusDi
         if (this.isPhone) {
             return <Block dataSet={this.state.dataSet}>
                 <Line>
-                    <ColumnIt width='10'></ColumnIt>
+                    <ColumnIt width='10' name=''></ColumnIt>
                     <Column code='Name_' width='90' name=''></Column>
                 </Line>
                 <Line>
                     <Column code='Mobile_' width='85' name='手机号'></Column>
                     <Column code='Opear_' width='15' name='' customText={(row: DataRow) => {
-                        return <span role='auiOpear' onClick={this.handleClick.bind(this, row)}>选择</span>
+                        return <span role='auiOpera' onClick={this.handleClick.bind(this, row)}>选择</span>
                     }}></Column>
                 </Line>
             </Block>
         } else {
             return <DBGrid dataSet={this.state.dataSet} onRowClick={this.handleClick.bind(this)}>
-                <ColumnIt></ColumnIt>
+                <ColumnIt width='15'></ColumnIt>
                 <Column code='Name_' width='50' name='收货人'></Column>
                 <Column code='Mobile_' width='50' name='手机号'></Column>
-                <Column code='Opear_' width='30' name='操作' customText={(row: DataRow) => {
+                <Column code='Opear_' width='30' name='操作' textAlign='center' customText={(row: DataRow) => {
                     return <span role='auiOpera'>选择</span>
                 }}></Column>
             </DBGrid>
@@ -80,7 +84,13 @@ export default class FSCusDialog extends BaseDialog<BaseDialogPropsType, FSCusDi
     }
 
     handleClick(row: DataRow) {
-        console.log(row);
+        let nameInput = document.getElementById(this.props.nameId) as HTMLInputElement;
+        nameInput.value = row.getString('Name_');
+        if(this.props.phoneId) {
+            let phoneInput = document.getElementById(this.props.phoneId) as HTMLInputElement;
+            phoneInput.value = row.getString('Mobile_');
+        }
+        this.handleClose();
     }
 
 }
