977999fe8f3346a485d5667faa2bfda9947d924f
胡红昌 <2220393782@qq.com>
2022-08-02 22:40:54 +0800

    完善支行匹配开窗

--- src/dialog/BankCorrectionDialog.tsx
+++ src/dialog/BankCorrectionDialog.tsx
@@ -1,24 +1,22 @@
-import { BaseDialog, BaseDialogPropsType, BaseDialogStateType, ColumnIt, DataRow, DataSet, DBEdit, DBGrid, SearchPanel } from "autumn-ui";
+import { BaseDialog, BaseDialogPropsType, BaseDialogStateType, Block, Column, ColumnIt, DataRow, DataSet, DBEdit, DBGrid, Line, SearchPanel } from "autumn-ui";
 import React from "react";
 import FplApi from "../api/FplApi";
-
-type BankCorrectionTypeProps = {
-    banknameInput: string
-} & Partial<BaseDialogPropsType>
+import { showMsg } from "../visualization/src/tool/Summer";
 
 type BankCorrectionTypeState = {
     dataRow: DataRow,
     dataSet: DataSet
 } & Partial<BaseDialogStateType>
 
-export default class BankCorrectionDialog extends BaseDialog<BankCorrectionTypeProps, BankCorrectionTypeState> {
-    constructor(props: BankCorrectionTypeProps) {
+export default class BankCorrectionDialog extends BaseDialog<BaseDialogPropsType, BankCorrectionTypeState> {
+    constructor(props: BaseDialogPropsType) {
         super(props);
         let dataRow = new DataRow();
-        dataRow.setValue('banknameInput', this.props.banknameInput);
+        let input = document.getElementById(this.props.inputId) as HTMLInputElement;
+        dataRow.setValue('banknameInput', input.value);
         this.state = {
             ...this.state,
-            width: '40rem',
+            width: '45rem',
             height: '30rem',
             dataRow,
             dataSet: new DataSet()
@@ -26,14 +24,12 @@ export default class BankCorrectionDialog extends BaseDialog<BankCorrectionTypeP
     }
 
     content(): JSX.Element {
-        return <React.Fragment>
+        return <div style={{ "height": "100%" }} role='content'>
             <SearchPanel dataRow={this.state.dataRow} onExecute={this.init.bind(this)}>
                 <DBEdit dataField='banknameInput' dataName='支行名称'></DBEdit>
             </SearchPanel>
-            <DBGrid dataSet={this.state.dataSet}>
-                <ColumnIt></ColumnIt>
-            </DBGrid>
-        </React.Fragment>
+            {this.getTable()}
+        </div>
     }
 
     componentWillMount() {
@@ -41,19 +37,51 @@ export default class BankCorrectionDialog extends BaseDialog<BankCorrectionTypeP
     }
 
     async init() {
-        if(!this.state.dataRow.getString('banknameInput')) {
+        if (this.state.dataRow.getString('banknameInput').length < 6) {
+            showMsg('请至少输入6个字的分行名称');
             return;
         }
         this.setLoad(true);
-        let ds = await FplApi.getBankCorrection(this.state.dataRow);
-        console.log(ds)
-        let dataSet = new DataSet();
-        let data = JSON.parse(ds.head.getString('data'));
-        // dataSet.append().setValue('Name_', data.bankname || )
+        let dataSet = await FplApi.getBankCorrection(this.state.dataRow);
         this.setState({
-            dataSet: ds
+            dataSet
         }, () => {
             this.setLoad(false);
         })
+    }
+
+    getTable() {
+        if (this.isPhone) {
+            return <Block dataSet={this.state.dataSet}>
+                <Line>
+                    <ColumnIt width='10' name=''></ColumnIt>
+                    <Column width='80' name='' code='XXX'></Column>
+                    <Column code='Opera' width='10' name='' customText={(row: DataRow) => {
+                        return <span role='auiOpera' onClick={this.handleClick.bind(this, row)}>选择</span>
+                    }}></Column>
+                </Line>
+                <Line>
+                    <Column width='50' name='支行号码' code='bankCode'></Column>
+                </Line>
+                <Line>
+                    <Column width='50' name='支行名称' code='bankName'></Column>
+                </Line>
+            </Block>
+        } else {
+            return <DBGrid dataSet={this.state.dataSet} onRowClick={this.handleClick.bind(this)}>
+                <ColumnIt width='10'></ColumnIt>
+                <Column code='bankCode' width='40' name="支行号码"></Column>
+                <Column code='bankName' width='60' name="支行名称"></Column>
+                <Column code='Opera' width='20' name='操作' textAlign="center" customText={(row: DataRow) => {
+                    return <span role='auiOpera'>选择</span>
+                }}></Column>
+            </DBGrid>
+        }
+    }
+
+    handleClick(row: DataRow) {
+        let input = document.getElementById(this.props.inputId) as HTMLInputElement;
+        input.value = row.getString('bankName');
+        this.handleClose();
     }
 }
