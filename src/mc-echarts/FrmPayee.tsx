357f4bb5352f0fd476aefd1931eb573d79b41ebe
李敏 <464004210@qq.com>
2022-08-05 16:01:25 +0800

    新增收款人MC

--- src/mc-echarts/FrmPayee.tsx
+++ src/mc-echarts/FrmPayee.tsx
@@ -1,71 +1,62 @@
 import { DataRow, DataSet, WebControl } from "autumn-ui";
-import * as echarts from "echarts";
 import React from "react";
-import UIIntroduction from "../module/UIIntroduction";
-import styles from "./FrmAPManageMC1.css";
+import styles from "./FrmPayee.css";
+import * as echarts from "echarts";
 import { MCChartColors } from "./FrmTaurusMC";
+import UIIntroduction from "../module/UIIntroduction";
 
-type FrmAPManageMC1TypeProps = {
+type FrmPayeeTypeProps = {
     dataJson: string,
     introduction: string
 }
 
-type FrmAPManageMC1TypeState = {
-    lineData: DataSet,
+type FrmPayeeTypeState = {
     dataJson: DataRow,
 }
 
-export default class FrmAPManageMC1 extends WebControl<FrmAPManageMC1TypeProps, FrmAPManageMC1TypeState> {
-    constructor(props: FrmAPManageMC1TypeProps) {
+export default class FrmPayee extends WebControl<FrmPayeeTypeProps, FrmPayeeTypeState> {
+    constructor(props: FrmPayeeTypeProps) {
         super(props);
-        let lineData = new DataSet();
         let lineRow = new DataRow();
-        lineData.append().setValue('Value_', 258).setValue('XName_', '周一');
-        lineData.append().setValue('Value_', 225).setValue('XName_', '周二');
-        lineData.append().setValue('Value_', 240).setValue('XName_', '周三');
-        lineData.append().setValue('Value_', 210).setValue('XName_', '周四');
-        lineData.append().setValue('Value_', 320).setValue('XName_', '周五');
-        lineData.append().setValue('Value_', 350).setValue('XName_', '周六');
-        lineData.append().setValue('Value_', 260).setValue('XName_', '周日');
         let dataJson: DataRow = lineRow.setJson(this.props.dataJson);
         this.state = {
-            lineData,
             dataJson: dataJson,
         }
     }
 
     render(): React.ReactNode {
         return <div className={styles.mc}>
-            <UIIntroduction introduction={this.props.introduction}></UIIntroduction>
+            {/* <UIIntroduction introduction={this.props.introduction}></UIIntroduction> */}
+            <UIIntroduction introduction={'车队长（收款人）应收冲应付,债权和债务进行冲销,也就是应收款与应付款之间的冲销,使用应收冲应付进行处理。'}></UIIntroduction>
             <div className={styles.mcMain}>
                 <div className={styles.mcFlowChartBox}>
                     <div className={styles.mcTitle}>流程图</div>
                     <div className={styles.mcFlowChartMain}>
                         <div className={styles.mcFlowChart}></div>
                         <div className={styles.mcFlowBox}>
-                            <div className={`${this.state.dataJson.getBoolean(`应付调整单_Dis`) ? styles.receipt_disable : styles.receipt} ${styles.stock2}`} onClick={this.linkTo.bind(this, '应付调整单')}>
-                                <span>应付调整单</span>
+                            <div className={`${this.state.dataJson.getBoolean(`收款人资料_Dis`) ? styles.register_disable : styles.register} ${styles.stock1}`} onClick={this.linkTo.bind(this, '收款人资料')}>
+                                <span>收款人资料</span>
                             </div>
-                            <div className={`${this.state.dataJson.getBoolean(`进货单_Dis`) ? styles.receipt_disable : styles.receipt} ${styles.stock4}`} onClick={this.linkTo.bind(this, '进货单')}>
-                                <span>进货单</span>
+                            <div className={`${this.state.dataJson.getBoolean(`钱包_Dis`) ? styles.control_disable : styles.control} ${styles.stock3}`} onClick={this.linkTo.bind(this, '钱包')}>
+                                <span>钱包</span>
                             </div>
-                            <div className={`${this.state.dataJson.getBoolean(`应付结账单_Dis`) ? styles.receipt_disable : styles.receipt} ${styles.stock5}`} onClick={this.linkTo.bind(this, '应付结账单')}>
-                                <span>应付结账单</span>
+                            <div className={`${this.state.dataJson.getBoolean(`应付_Dis`) ? styles.control_disable : styles.control} ${styles.stock4}`} onClick={this.linkTo.bind(this, '应付')}>
+                                <span>应付</span>
                             </div>
-                            <div className={`${this.state.dataJson.getBoolean(`应付对账单_Dis`) ? styles.receipt_disable : styles.receipt} ${styles.stock7}`} onClick={this.linkTo.bind(this, '应付对账单')}>
-                                <span>应付对账单</span>
+                            <div className={`${this.state.dataJson.getBoolean(`应收_Dis`) ? styles.control_disable : styles.control} ${styles.stock5}`} onClick={this.linkTo.bind(this, '应收')}>
+                                <span>应收</span>
                             </div>
-                            <div className={`${this.state.dataJson.getBoolean(`应收账款_Dis`) ? styles.control_disable : styles.control} ${styles.stock8}`} onClick={this.linkTo.bind(this, '应收账款')}>
-                                <span>应收账款</span>
+                            <div className={`${this.state.dataJson.getBoolean(`司机_Dis`) ? styles.other_disable : styles.other} ${styles.stock6}`} onClick={this.linkTo.bind(this, '司机')}>
+                                <span>司机</span>
                             </div>
-                            <div className={`${this.state.dataJson.getBoolean(`付款(申请)单_Dis`) ? styles.receipt_disable : styles.receipt} ${styles.stock11}`} onClick={this.linkTo.bind(this, '付款(申请)单')}>
-                                <span>付款(申请)单</span>
+                            <div className={`${this.state.dataJson.getBoolean(`网货_Dis`) ? styles.other_disable : styles.other} ${styles.stock7}`} onClick={this.linkTo.bind(this, '网货')}>
+                                <span>网货</span>
                             </div>
-                            <div className={`${this.state.dataJson.getBoolean(`会计凭证_Dis`) ? styles.other_disable : styles.other} ${styles.stock12}`} onClick={this.linkTo.bind(this, '会计凭证')}>
-                                <span>会计凭证</span>
+                            <div className={`${this.state.dataJson.getBoolean(`网货_Dis`) ? styles.other_disable : styles.other} ${styles.stock7}`} onClick={this.linkTo.bind(this, '网货')}>
+                                <span>网货</span>
                             </div>
-                            <div className={`${this.state.dataJson.getBoolean(`银行存款_Dis`) ? styles.other_disable : styles.other} ${styles.stock14}`} onClick={this.linkTo.bind(this, '银行存款')}>
-                                <span>银行存款</span>
+                            <div className={`${styles.MCtext} ${styles.MCtextStock1}`}>
+                                <span>网货公司</span>
                             </div>
                         </div>
                     </div>
@@ -74,44 +65,44 @@ export default class FrmAPManageMC1 extends WebControl<FrmAPManageMC1TypeProps, 
                     <div className={styles.content}>
                         <ul>
                             <li>
-                                <p>到期应付</p>
-                                <div className={styles.links_skin} onClick={this.gotoFun.bind(this, '到期应付')}>
+                                <p>应收款</p>
+                                <div className={styles.links_skin} onClick={this.gotoFun.bind(this, '应收款')}>
                                     <span>{0}</span>
                                     <span>元</span>
                                 </div>
                             </li>
                             <li>
-                                <p>本期应付</p>
-                                <div className={styles.links_skin} onClick={this.gotoFun.bind(this, '本期应付')}>
+                                <p>已收款</p>
+                                <div className={styles.links_skin} onClick={this.gotoFun.bind(this, '已收款')}>
                                     <span>{0}</span>
                                     <span>元</span>
                                 </div>
                             </li>
                             <li>
-                                <p>本期已付</p>
-                                <div className={styles.links_skin} onClick={this.gotoFun.bind(this, '本期已付')}>
+                                <p>未收款</p>
+                                <div className={styles.links_skin} onClick={this.gotoFun.bind(this, '未收款')}>
                                     <span>{0}</span>
                                     <span>元</span>
                                 </div>
                             </li>
                             <li>
-                                <p>期末应付</p>
-                                <div>
-                                    <span>0</span>
+                                <p>已付款</p>
+                                <div className={styles.links_skin} onClick={this.gotoFun.bind(this, '已付款')}>
+                                    <span>{0}</span>
                                     <span>元</span>
                                 </div>
                             </li>
                             <li>
-                                <p>逾期应付</p>
-                                <div>
-                                    <span>0</span>
+                                <p>未付款</p>
+                                <div className={styles.links_skin} onClick={this.gotoFun.bind(this, '未付款')}>
+                                    <span>{0}</span>
                                     <span>元</span>
                                 </div>
                             </li>
                             <li>
-                                <p>新增应付客户</p>
-                                <div>
-                                    <span>0</span>
+                                <p>钱包余额</p>
+                                <div className={styles.links_skin} onClick={this.gotoFun.bind(this, '钱包余额')}>
+                                    <span>{0}</span>
                                     <span>元</span>
                                 </div>
                             </li>
@@ -135,18 +126,20 @@ export default class FrmAPManageMC1 extends WebControl<FrmAPManageMC1TypeProps, 
         let lineChart = document.querySelector(`.${styles.FrmTaurusMCLine}`) as HTMLDivElement;
         let myChart = echarts.init(lineChart);
         let ds = new DataSet();
-        ds.appendDataSet(this.state.lineData);
         ds.first();
-        let xArr = [];
-        let sData = [];
-        while (ds.fetch()) {
-            xArr.push(ds.getString('XName_'));
-            sData.push(ds.getDouble('Value_'));
-        }
+        let xArr = [
+            '一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'
+        ];
+        let sData1 = [12, 51, 41, 12, 44, 11, 44, 77, 11, 44, 45, 5];
+        let sData2 = [44, 12, 44, 11, 77, 11, 44, 45, 5, 12, 51, 41];
+
+        // while (ds.fetch()) {
+        //     sData.push(ds.getDouble('Value_'));
+        // }
         let option = {
             xAxis: {
                 type: 'category',
-                data: ['产品部', '人事部', '营销部', '设计部', '技术部'],
+                data: xArr,
                 axisLabel: {
                     color: '#333333'
                 },
@@ -172,13 +165,29 @@ export default class FrmAPManageMC1 extends WebControl<FrmAPManageMC1TypeProps, 
             },
             series: [
                 {
-                    data: sData,
+                    data: sData1,
                     type: 'bar',
                     itemStyle: {
                         color: MCChartColors[0]
                     },
+                    barWidth: 20,
                     lineStyle: {
                         color: MCChartColors[0]
+                    },
+                    label: {
+                        show: true,
+                        position: 'top'
+                    },
+                },
+                {
+                    data: sData2,
+                    type: 'bar',
+                    itemStyle: {
+                        color: MCChartColors[1]
+                    },
+                    barWidth: 20,
+                    lineStyle: {
+                        color: MCChartColors[1]
                     },
                     label: {
                         show: true,
@@ -199,56 +208,35 @@ export default class FrmAPManageMC1 extends WebControl<FrmAPManageMC1TypeProps, 
             nodes,
             linesData: [
                 {
-                    coords: [ //应付调整单 往下线条
-                        [168, 75],
-                        [168, 108]
+                    coords: [ //收款人往下线条
+                        [111, 75],
+                        [111, 105],
                     ]
                 },
                 {
-                    coords: [ //进货单 往右线条
-                        [73, 123],
-                        [150, 123],
+                    coords: [ // 钱包往下线条
+                        [111, 160],
+                        [111, 185],
                     ]
                 },
                 {
-                    coords: [ //应付结账单 往右下线条
-                        [190, 123],
-                        [279, 123],
-                        [279, 271]
+                    coords: [  //钱包往右线条
+                        [133, 123],
+                        [198, 123],
                     ]
                 },
                 {
-                    coords: [ //应付结账单 往下线条
-                        [168, 160],
-                        [168, 189]
+                    coords: [ //应付往下线条
+                        [221, 160],
+                        [221, 185]
                     ]
                 },
                 {
-                    coords: [ //应付结账单 往左下线条
-                        [168, 174],
-                        [50, 174],
-                        [50, 189]
+                    coords: [ //应收往下线条
+                        [111, 244],
+                        [111, 275]
                     ]
-                },
-                {
-                    coords: [ //应收账款 往下线条
-                        [168, 247],
-                        [168, 271]
-                    ]
-                },
-                {
-                    coords: [ //付款(申请)单 往右线条
-                        [190, 290],
-                        [256, 290]
-                    ]
-                },
-                {
-                    coords: [ //付款(申请)单 往下线条
-                        [168, 328],
-                        [168, 349]
-                    ]
-                },
-
+                }
             ]
         }
 
