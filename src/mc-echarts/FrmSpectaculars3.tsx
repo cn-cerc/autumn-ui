e9ff962dad69a6bba7cf9340a4a8ea75832adfa1
李敏 <464004210@qq.com>
2022-08-10 17:02:26 +0800

    Update

--- src/mc-echarts/FrmSpectaculars3.tsx
+++ src/mc-echarts/FrmSpectaculars3.tsx
@@ -402,19 +402,12 @@ export default class FrmSpectaculars3 extends WebControl<FrmSpectaculars3TypePro
     initPieChart1() {
         let peiChart = document.querySelector(`.${styles.FrmSpectaculars3MCPie2}`) as HTMLDivElement;
         let myChart = echarts.init(peiChart);
-        let ydeal = 0, ndeal = 0;
-        if (this.state.dealStatus.getValue('ydeal') != null) {
-            ydeal = this.state.dealStatus.getDouble('ydeal');
-        }
-        if (this.state.dealStatus.getValue('ndeal') != null) {
-            ndeal = this.state.dealStatus.getDouble('ndeal');
-        }
+        console.log(this.state.dealStatus);
         let dataArr: any = [];
         dataArr = [
-            { value: ydeal, name: '已成交' },
-            { value: ndeal, name: '未成交' },
-        ]
-
+            { value: this.state.dealStatus.getDouble('ydeal'), name: '已成交' },
+            { value: this.state.dealStatus.getDouble('ndeal'), name: '未成交' },
+        ];
         let option = {
             tooltip: {
                 trigger: 'item'
