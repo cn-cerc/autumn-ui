eca2cbdb4f67340e39f2b179bebdd2162bc65787
郑振强 <13979669972@163.com>
2022-08-25 20:23:55 +0800

    Update FrmChargeManage.tsx

--- src/mc-echarts/FrmChargeManage.tsx
+++ src/mc-echarts/FrmChargeManage.tsx
@@ -5,16 +5,17 @@ import UIIntroduction from "../module/UIIntroduction";
 import styles from "./FrmChargeManage.css";
 import { MCChartColors } from "./FrmTaurusMC";
 
+// diteng-finance
 type FrmChargeManageTypeProps = {
-    dataJson: string,
-    introduction: string
+
 }
 
 type FrmChargeManageTypeState = {
     lineData: DataSet,
     pieData1: DataSet
     pieData2: DataSet,
-    dataJson: DataRow,
+    dataRow: DataRow,
+    introduction: string
 }
 
 export default class FrmChargeManage extends WebControl<FrmChargeManageTypeProps, FrmChargeManageTypeState> {
@@ -39,52 +40,74 @@ export default class FrmChargeManage extends WebControl<FrmChargeManageTypeProps
         pieData2.append().setValue('Value_', 20).setValue('Name_', '广西省');
         pieData2.append().setValue('Value_', 30).setValue('Name_', '湖南省');
         pieData2.append().setValue('Value_', 15).setValue('Name_', '广东省');
-        let dataJson: DataRow = lineRow.setJson(this.props.dataJson);
+        let dataRow: DataRow = lineRow.setValue("费用请购单_URL", "")
+            .setValue("费用请购单_Dis", false)
+            .setValue("费用报销单_URL", "")
+            .setValue("费用报销单_Dis", false)
+            .setValue("费用进货单_URL", "")
+            .setValue("费用进货单_Dis", false)
+            .setValue("付款(申请)单_URL", "")
+            .setValue("付款(申请)单_Dis", false)
+            .setValue("报废单_URL", "")
+            .setValue("报废单_Dis", false)
+            .setValue("办公用品库存表_URL", "")
+            .setValue("办公用品库存表_Dis", false)
+            .setValue("会计凭证_URL", "TFrmAccBook")
+            .setValue("会计凭证_Dis", false)
+            .setValue("还库单_URL", "")
+            .setValue("还库单_Dis", false)
+            .setValue("领用单_URL", "")
+            .setValue("领用单_Dis", false)
+            .setValue("个人领用报表_URL", "")
+            .setValue("个人领用报表_Dis", false);
+        let introduction = "本模组主要提供人事资料的管理，由于每一家企业的性质与规模不同，此模组在实际使用时，会根据每家企业的特点与需求进行定制开发。当前模组默认的只是提供了最为基础的数据管理，并为定制开发做好了准备。";
+
         this.state = {
             lineData,
             pieData1,
             pieData2,
-            dataJson: dataJson,
+            dataRow,
+            introduction
         }
     }
 
     render(): React.ReactNode {
         return <div className={styles.mc}>
-            <UIIntroduction introduction={this.props.introduction}></UIIntroduction>
+            <UIIntroduction introduction={this.state.introduction}></UIIntroduction>
             <div className={styles.mcMain}>
                 <div className={styles.mcFlowChartBox}>
                     <div className={styles.mcTitle}>流程图</div>
                     <div className={styles.mcFlowChartMain}>
                         <div className={styles.mcFlowChart}></div>
                         <div className={styles.mcFlowBox}>
-                            <div className={`${this.state.dataJson.getBoolean('费用请购单_Dis') ? styles.receipt_disable : styles.receipt} ${styles.stock2}`} onClick={this.linkTo.bind(this, '费用请购单')}>
+                            <div className={`${this.state.dataRow.getBoolean('费用请购单_Dis') ? styles.receipt_disable : styles.receipt} ${styles.stock2}`} onClick={this.linkTo.bind(this, '费用请购单')}>
                                 <span>费用请购单</span>
                             </div>
-                            <div className={`${this.state.dataJson.getBoolean('费用报销单_Dis') ? styles.receipt_disable : styles.receipt} ${styles.stock3}`} onClick={this.linkTo.bind(this, '费用报销单')}>
+                            <div className={`${this.state.dataRow.getBoolean('费用报销单_Dis') ? styles.receipt_disable : styles.receipt} ${styles.stock3}`} onClick={this.linkTo.bind(this, '费用报销单')}>
                                 <span>费用报销单</span>
                             </div>
-                            <div className={`${this.state.dataJson.getBoolean('费用进货单_Dis') ? styles.receipt_disable : styles.receipt} ${styles.stock5}`} onClick={this.linkTo.bind(this, '费用进货单')}>
+                            <div className={`${this.state.dataRow.getBoolean('费用进货单_Dis') ? styles.receipt_disable : styles.receipt} ${styles.stock5}`} onClick={this.linkTo.bind(this, '费用进货单')}>
                                 <span>费用进货单</span>
                             </div>
-                            <div className={`${this.state.dataJson.getBoolean('付款(申请)单_Dis') ? styles.receipt_disable : styles.receipt} ${styles.stock6}`} onClick={this.linkTo.bind(this, '付款(申请)单')}>
+                            <div className={`${this.state.dataRow.getBoolean('付款(申请)单_Dis') ? styles.receipt_disable : styles.receipt} ${styles.stock6}`} onClick={this.linkTo.bind(this, '付款(申请)单')}>
                                 <span>付款(申请)单</span>
                             </div>
-                            <div className={`${this.state.dataJson.getBoolean('报废单_Dis') ? styles.receipt_disable : styles.receipt} ${styles.stock7}`} onClick={this.linkTo.bind(this, '报废单')}>
+                            <div className={`${this.state.dataRow.getBoolean('报废单_Dis') ? styles.receipt_disable : styles.receipt} ${styles.stock7}`} onClick={this.linkTo.bind(this, '报废单')}>
                                 <span>报废单</span>
                             </div>
-                            <div className={`${this.state.dataJson.getBoolean('办公用品库存表_Dis') ? styles.control_disable : styles.control} ${styles.stock8}`} onClick={this.linkTo.bind(this, '办公用品库存表')}>
+                            <div className={`${this.state.dataRow.getBoolean('办公用品库存表_Dis') ? styles.control_disable : styles.control} ${styles.stock8}`} onClick={this.linkTo.bind(this, '办公用品库存表')}>
                                 <span>办公用品库存表</span>
                             </div>
-                            <div className={`${this.state.dataJson.getBoolean('会计凭证_Dis') ? styles.other_disable : styles.other} ${styles.stock9}`} onClick={this.linkTo.bind(this, '会计凭证')}>
+                            <div className={`${this.state.dataRow.getBoolean('会计凭证_Dis') ? styles.other_disable : styles.other} ${styles.stock9}`} onClick={this.linkTo.bind(this, '会计凭证')}>
                                 <span>会计凭证</span>
                             </div>
-                            <div className={`${this.state.dataJson.getBoolean('还库单_Dis') ? styles.receipt_disable : styles.receipt} ${styles.stock10}`} onClick={this.linkTo.bind(this, '还库单')}>
+                            <div className={`${this.state.dataRow.getBoolean('还库单_Dis') ? styles.receipt_disable : styles.receipt} ${styles.stock10}`} onClick={this.linkTo.bind(this, '还库单')}>
                                 <span>还库单</span>
                             </div>
-                            <div className={`${this.state.dataJson.getBoolean('领用单_Dis') ? styles.receipt_disable : styles.receipt} ${styles.stock11}`} onClick={this.linkTo.bind(this, '领用单')}>
+                            <div className={`${this.state.dataRow.getBoolean('领用单_Dis') ? styles.receipt_disable : styles.receipt} ${styles.stock11}`} onClick={this.linkTo.bind(this, '领用单')}>
                                 <span>领用单</span>
                             </div>
-                            <div className={`${this.state.dataJson.getBoolean('个人领用报表_Dis') ? styles.control_disable : styles.control} ${styles.stock14}`} onClick={this.linkTo.bind(this, '个人领用报表')}>
+                            <div className={`${this.state.dataRow.getBoolean('个人领用报表_Dis') ? styles.control_disable : styles.control} ${styles.stock14}`} onClick={this.linkTo.bind(this, '个人领用报表')}>
                                 <span>个人领用报表</span>
                             </div>
                         </div>
@@ -421,8 +444,8 @@ export default class FrmChargeManage extends WebControl<FrmChargeManageTypeProps
     }
 
     linkTo(name: string) {
-        if (!this.state.dataJson.getBoolean(`${name}_Dis`)) {
-            location.href = this.state.dataJson.getString(`${name}_URL`);
+        if (!this.state.dataRow.getBoolean(`${name}_Dis`)) {
+            location.href = this.state.dataRow.getString(`${name}_URL`);
         }
     }
 }
