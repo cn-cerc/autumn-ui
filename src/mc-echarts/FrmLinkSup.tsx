d24903c7160f172244fcf582096169e04f53f46b
HUANG Rongjun <l1091462907@gmail.com>
2022-08-25 22:31:46 +0800

    Update FrmLinkSup.tsx

--- src/mc-echarts/FrmLinkSup.tsx
+++ src/mc-echarts/FrmLinkSup.tsx
@@ -15,31 +15,28 @@ type FrmLinkSupTypeState = {
   introduction: string;
 };
 
-export default class FrmLinkSup extends WebControl<
-  FrmLinkSupTypeProps,
-  FrmLinkSupTypeState
-> {
+export default class FrmLinkSup extends WebControl<FrmLinkSupTypeProps, FrmLinkSupTypeState> {
   constructor(props: FrmLinkSupTypeProps) {
     super(props);
     let lineData = new DataSet();
     let lineRow = new DataRow();
-    lineData.append().setValue("Value_", 258).setValue("XName_", "周一");
-    lineData.append().setValue("Value_", 225).setValue("XName_", "周二");
-    lineData.append().setValue("Value_", 240).setValue("XName_", "周三");
-    lineData.append().setValue("Value_", 210).setValue("XName_", "周四");
-    lineData.append().setValue("Value_", 320).setValue("XName_", "周五");
-    lineData.append().setValue("Value_", 350).setValue("XName_", "周六");
-    lineData.append().setValue("Value_", 260).setValue("XName_", "周日");
+    lineData.append().setValue('Value_', 258).setValue('XName_', '周一');
+    lineData.append().setValue('Value_', 225).setValue('XName_', '周二');
+    lineData.append().setValue('Value_', 240).setValue('XName_', '周三');
+    lineData.append().setValue('Value_', 210).setValue('XName_', '周四');
+    lineData.append().setValue('Value_', 320).setValue('XName_', '周五');
+    lineData.append().setValue('Value_', 350).setValue('XName_', '周六');
+    lineData.append().setValue('Value_', 260).setValue('XName_', '周日');
     let pieData1 = new DataSet();
-    pieData1.append().setValue("Value_", 10).setValue("Name_", "1-3吨");
-    pieData1.append().setValue("Value_", 20).setValue("Name_", "3-5吨");
-    pieData1.append().setValue("Value_", 30).setValue("Name_", "5-7吨");
-    pieData1.append().setValue("Value_", 15).setValue("Name_", "7-9吨");
+    pieData1.append().setValue('Value_', 10).setValue('Name_', '1-3吨');
+    pieData1.append().setValue('Value_', 20).setValue('Name_', '3-5吨');
+    pieData1.append().setValue('Value_', 30).setValue('Name_', '5-7吨');
+    pieData1.append().setValue('Value_', 15).setValue('Name_', '7-9吨');
     let pieData2 = new DataSet();
-    pieData2.append().setValue("Value_", 11).setValue("Name_", "微型卡车");
-    pieData2.append().setValue("Value_", 13).setValue("Name_", "轻型卡车");
-    pieData2.append().setValue("Value_", 18).setValue("Name_", "中型卡车");
-    pieData2.append().setValue("Value_", 20).setValue("Name_", "重型卡车");
+    pieData2.append().setValue('Value_', 11).setValue('Name_', '微型卡车');
+    pieData2.append().setValue('Value_', 13).setValue('Name_', '轻型卡车');
+    pieData2.append().setValue('Value_', 18).setValue('Name_', '中型卡车');
+    pieData2.append().setValue('Value_', 20).setValue('Name_', '重型卡车');
     let dataRow: DataRow = lineRow
       .setValue("审核厂商接入_URL", "FrmAuditSupAccess")
       .setValue("审核厂商接入_Dis", false)
@@ -61,101 +58,57 @@ export default class FrmLinkSup extends WebControl<
       pieData2,
       dataRow,
       introduction,
-    };
+    }
   }
 
   render(): React.ReactNode {
-    return (
-      <div className={styles.mc}>
-        <UIIntroduction introduction={this.state.introduction}></UIIntroduction>
-        <div className={styles.mcMain}>
-          <div className={styles.mcFlowChartBox}>
-            <div className={styles.mcTitle}>流程图</div>
-            <div className={styles.mcFlowChartMain}>
-              <div className={styles.mcFlowChart}></div>
-              <div className={styles.mcFlowBox}>
-                <div
-                  className={`${
-                    this.state.dataRow.getBoolean(`审核厂商接入_Dis`)
-                      ? styles.other_disable
-                      : styles.other
-                  } ${styles.stock2}`}
-                  onClick={this.linkTo.bind(this, "审核厂商接入")}
-                >
-                  <span>审核厂商接入</span>
-                </div>
-                <div
-                  className={`${
-                    this.state.dataRow.getBoolean(`连接厂商申请_Dis`)
-                      ? styles.other_disable
-                      : styles.other
-                  } ${styles.stock4}`}
-                  onClick={this.linkTo.bind(this, "连接厂商申请")}
-                >
-                  <span>连接厂商申请</span>
-                </div>
-                <div
-                  className={`${
-                    this.state.dataRow.getBoolean(`上游互联管理_Dis`)
-                      ? styles.control_disable
-                      : styles.control
-                  } ${styles.stock5}`}
-                  onClick={this.linkTo.bind(this, "上游互联管理")}
-                >
-                  <span>上游互联管理</span>
-                </div>
-                <div
-                  className={`${
-                    this.state.dataRow.getBoolean(`厂商报价审核_Dis`)
-                      ? styles.control_disable
-                      : styles.control
-                  } ${styles.stock6}`}
-                  onClick={this.linkTo.bind(this, "厂商报价审核")}
-                >
-                  <span>厂商报价审核</span>
-                </div>
-                <div
-                  className={`${
-                    this.state.dataRow.getBoolean(`接入权限管理_Dis`)
-                      ? styles.control_disable
-                      : styles.control
-                  } ${styles.stock7}`}
-                  onClick={this.linkTo.bind(this, "接入权限管理")}
-                >
-                  <span>接入权限管理</span>
-                </div>
-                <div
-                  className={`${
-                    this.state.dataRow.getBoolean(`委外生产进度表_Dis`)
-                      ? styles.control_disable
-                      : styles.control
-                  } ${styles.stock8}`}
-                  onClick={this.linkTo.bind(this, "委外生产进度表")}
-                >
-                  <span>委外生产进度表</span>
-                </div>
+    return <div className={styles.mc}>
+      <UIIntroduction introduction={this.state.introduction}></UIIntroduction>
+      <div className={styles.mcMain}>
+        <div className={styles.mcFlowChartBox}>
+          <div className={styles.mcTitle}>流程图</div>
+          <div className={styles.mcFlowChartMain}>
+            <div className={styles.mcFlowChart}></div>
+            <div className={styles.mcFlowBox}>
+              <div className={`${this.state.dataRow.getBoolean(`审核厂商接入_Dis`) ? styles.other_disable : styles.other} ${styles.stock2}`} onClick={this.linkTo.bind(this, '审核厂商接入')}>
+                <span>审核厂商接入</span>
               </div>
-            </div>
-          </div>
-          <div className={styles.mcCharts}>
-            <div className={styles.mcPieChart}>
-              <div className={styles.mcPieBox1}>
-                <div className={styles.mcTitle}>比例图（对接中）</div>
-                <div className={styles.FrmTaurusMCPie1}></div>
+              <div className={`${this.state.dataRow.getBoolean(`连接厂商申请_Dis`) ? styles.other_disable : styles.other} ${styles.stock4}`} onClick={this.linkTo.bind(this, '连接厂商申请')}>
+                <span>连接厂商申请</span>
               </div>
-              <div className={styles.mcPieBox2}>
-                <div className={styles.mcTitle}>比例图（对接中）</div>
-                <div className={styles.FrmTaurusMCPie2}></div>
+              <div className={`${this.state.dataRow.getBoolean(`上游互联管理_Dis`) ? styles.control_disable : styles.control} ${styles.stock5}`} onClick={this.linkTo.bind(this, '上游互联管理')}>
+                <span>上游互联管理</span>
               </div>
-            </div>
-            <div className={styles.mcTrendChart}>
-              <div className={styles.mcTitle}>比例图（对接中）</div>
-              <div className={styles.FrmTaurusMCLine}></div>
+              <div className={`${this.state.dataRow.getBoolean(`厂商报价审核_Dis`) ? styles.control_disable : styles.control} ${styles.stock6}`} onClick={this.linkTo.bind(this, '厂商报价审核')}>
+                <span>厂商报价审核</span>
+              </div>
+              <div className={`${this.state.dataRow.getBoolean(`接入权限管理_Dis`) ? styles.control_disable : styles.control} ${styles.stock7}`} onClick={this.linkTo.bind(this, '接入权限管理')}>
+                <span>接入权限管理</span>
+              </div>
+              <div className={`${this.state.dataRow.getBoolean(`委外生产进度表_Dis`) ? styles.control_disable : styles.control} ${styles.stock8}`} onClick={this.linkTo.bind(this, '委外生产进度表')}>
+                <span>委外生产进度表</span>
+              </div>
             </div>
           </div>
         </div>
+        <div className={styles.mcCharts}>
+          <div className={styles.mcPieChart}>
+            <div className={styles.mcPieBox1}>
+              <div className={styles.mcTitle}>比例图（对接中）</div>
+              <div className={styles.FrmTaurusMCPie1}></div>
+            </div>
+            <div className={styles.mcPieBox2}>
+              <div className={styles.mcTitle}>比例图（对接中）</div>
+              <div className={styles.FrmTaurusMCPie2}></div>
+            </div>
+          </div>
+          <div className={styles.mcTrendChart}>
+            <div className={styles.mcTitle}>比例图（对接中）</div>
+            <div className={styles.FrmTaurusMCLine}></div>
+          </div>
+        </div>
       </div>
-    );
+    </div>
   }
 
   componentDidMount(): void {
@@ -166,9 +119,7 @@ export default class FrmLinkSup extends WebControl<
   }
 
   initBarChart() {
-    let lineChart = document.querySelector(
-      `.${styles.FrmTaurusMCLine}`
-    ) as HTMLDivElement;
+    let lineChart = document.querySelector(`.${styles.FrmTaurusMCLine}`) as HTMLDivElement;
     let myChart = echarts.init(lineChart);
     let ds = new DataSet();
     ds.appendDataSet(this.state.lineData);
@@ -176,27 +127,27 @@ export default class FrmLinkSup extends WebControl<
     let xArr = [];
     let sData = [];
     while (ds.fetch()) {
-      xArr.push(ds.getString("XName_"));
-      sData.push(ds.getDouble("Value_"));
+      xArr.push(ds.getString('XName_'));
+      sData.push(ds.getDouble('Value_'));
     }
     let option = {
       xAxis: {
-        type: "category",
-        data: ["产品部", "人事部", "营销部", "设计部", "技术部"],
+        type: 'category',
+        data: ['产品部', '人事部', '营销部', '设计部', '技术部'],
         axisLabel: {
-          color: "#333333",
+          color: '#333333'
         },
         axisLine: {
           lineStyle: {
-            color: "#333333",
-          },
-        },
+            color: '#333333'
+          }
+        }
       },
       yAxis: {
-        type: "value",
+        type: 'value',
         axisLabel: {
-          color: "#333333",
-        },
+          color: '#333333'
+        }
       },
       tooltip: {},
       grid: {
@@ -209,29 +160,27 @@ export default class FrmLinkSup extends WebControl<
       series: [
         {
           data: sData,
-          type: "bar",
-          name: "售出",
+          type: 'bar',
+          name: '售出',
           itemStyle: {
-            color: MCChartColors[0],
+            color: MCChartColors[0]
           },
           lineStyle: {
-            color: MCChartColors[0],
+            color: MCChartColors[0]
           },
           label: {
             show: true,
-            position: "top",
+            position: 'top'
           },
-        },
-      ],
+        }
+      ]
     };
     //@ts-ignore
     myChart.setOption(option);
   }
 
   initPieChart1() {
-    let peiChart = document.querySelector(
-      `.${styles.FrmTaurusMCPie1}`
-    ) as HTMLDivElement;
+    let peiChart = document.querySelector(`.${styles.FrmTaurusMCPie1}`) as HTMLDivElement;
     let myChart = echarts.init(peiChart);
     let ds = new DataSet();
     ds.appendDataSet(this.state.pieData1);
@@ -239,21 +188,21 @@ export default class FrmLinkSup extends WebControl<
     let dataArr = [];
     while (ds.fetch()) {
       dataArr.push({
-        name: ds.getString("Name_"),
-        value: ds.getDouble("Value_"),
-      });
+        name: ds.getString('Name_'),
+        value: ds.getDouble('Value_')
+      })
     }
     let option = {
       tooltip: {
-        trigger: "item",
+        trigger: 'item'
       },
       legend: {
-        top: "25%",
-        left: "65%",
-        orient: "vertical",
+        top: '25%',
+        left: '65%',
+        orient: 'vertical',
         itemWidth: 8,
         itemHeight: 8,
-        icon: "circle",
+        icon: 'circle',
       },
       grid: {
         top: 40,
@@ -265,36 +214,34 @@ export default class FrmLinkSup extends WebControl<
       series: [
         {
           // name: '本周货运吨数占比',
-          type: "pie",
-          center: ["30%", "50%"],
-          radius: ["40%", "70%"],
+          type: 'pie',
+          center: ['30%', '50%'],
+          radius: ['40%', '70%'],
           avoidLabelOverlap: false,
           label: {
             show: false,
-            position: "center",
+            position: 'center'
           },
           emphasis: {
             label: {
               show: true,
-              fontSize: "20",
-              fontWeight: "bold",
-            },
+              fontSize: '20',
+              fontWeight: 'bold'
+            }
           },
           labelLine: {
-            show: false,
+            show: false
           },
-          data: dataArr,
-        },
-      ],
-    };
+          data: dataArr
+        }
+      ]
+    }
     //@ts-ignore
     myChart.setOption(option);
   }
 
   initPieChart2() {
-    let peiChart = document.querySelector(
-      `.${styles.FrmTaurusMCPie2}`
-    ) as HTMLDivElement;
+    let peiChart = document.querySelector(`.${styles.FrmTaurusMCPie2}`) as HTMLDivElement;
     let myChart = echarts.init(peiChart);
     let ds = new DataSet();
     ds.appendDataSet(this.state.pieData2);
@@ -302,97 +249,90 @@ export default class FrmLinkSup extends WebControl<
     let dataArr = [];
     while (ds.fetch()) {
       dataArr.push({
-        name: ds.getString("Name_"),
-        value: ds.getDouble("Value_"),
-      });
+        name: ds.getString('Name_'),
+        value: ds.getDouble('Value_')
+      })
     }
     let option = {
       tooltip: {
-        trigger: "item",
+        trigger: 'item'
       },
       legend: {
-        top: "25%",
-        left: "65%",
-        orient: "vertical",
+        top: '25%',
+        left: '65%',
+        orient: 'vertical',
         itemWidth: 8,
         itemHeight: 8,
-        icon: "circle",
+        icon: 'circle',
       },
       series: [
         {
           // name: '本周货运车辆占比',
-          type: "pie",
-          center: ["30%", "50%"],
-          radius: ["40%", "70%"],
+          type: 'pie',
+          center: ['30%', '50%'],
+          radius: ['40%', '70%'],
           avoidLabelOverlap: false,
           label: {
             show: false,
-            position: "center",
+            position: 'center'
           },
           emphasis: {
             label: {
               show: true,
-              fontSize: "20",
-              fontWeight: "bold",
-            },
+              fontSize: '20',
+              fontWeight: 'bold'
+            }
           },
           labelLine: {
-            show: false,
+            show: false
           },
-          data: dataArr,
-        },
-      ],
-    };
+          data: dataArr
+        }
+      ]
+    }
     //@ts-ignore
     myChart.setOption(option);
   }
 
   initFlowChart() {
-    let flowChart = document.querySelector(
-      `.${styles.mcFlowChart}`
-    ) as HTMLDivElement;
+    let flowChart = document.querySelector(`.${styles.mcFlowChart}`) as HTMLDivElement;
     let myChart = echarts.init(flowChart);
     let nodes: any[] = [];
     let charts = {
       nodes,
       linesData: [
         {
-          coords: [
-            //审核厂商接入 往下线条
+          coords: [ //审核厂商接入 往下线条
             [168, 75],
-            [168, 108],
-          ],
+            [168, 108]
+          ]
         },
         {
-          coords: [
-            //连接厂商申请 往右线条
+          coords: [ //连接厂商申请 往右线条
             [73, 123],
             [150, 123],
-          ],
+          ]
         },
         {
-          coords: [
-            //厂商报价审核 往右线条
+          coords: [ //厂商报价审核 往右线条
             [190, 123],
             [256, 123],
-          ],
+          ]
         },
         {
-          coords: [
-            //连接厂商申请 往下线条
+          coords: [ //连接厂商申请 往下线条
             [50, 160],
-            [50, 189],
-          ],
+            [50, 189]
+          ]
         },
         {
-          coords: [
-            //上游互联管理 往下线条
+          coords: [ //上游互联管理 往下线条
             [168, 160],
-            [168, 189],
-          ],
-        },
-      ],
-    };
+            [168, 189]
+          ]
+        }
+      ]
+    }
 
     let option = {
       backgroundColor: "",
@@ -400,59 +340,56 @@ export default class FrmLinkSup extends WebControl<
         min: 0,
         max: 328,
         show: false,
-        type: "value",
-        position: "top",
+        type: 'value',
+        position: 'top',
       },
       yAxis: {
         min: 0,
         max: function (val: number) {
-          return flowChart.offsetHeight;
+          return flowChart.offsetHeight
         },
         show: false,
-        type: "value",
+        type: 'value',
         inverse: true,
       },
       grid: {
         left: 0,
         right: 0,
         bottom: 0,
-        top: 0,
+        top: 0
       },
-      series: [
-        {
-          type: "graph",
-          coordinateSystem: "cartesian2d",
-          label: {
-            show: true,
-            position: "bottom",
-            color: "#fff",
-            formatter: function (item: any) {
-              return item.data.nodeName;
-            },
-          },
-          data: charts.nodes,
+      series: [{
+        type: 'graph',
+        coordinateSystem: 'cartesian2d',
+        label: {
+          show: true,
+          position: 'bottom',
+          color: '#fff',
+          formatter: function (item: any) {
+            return item.data.nodeName
+          }
         },
-        {
-          type: "lines",
-          polyline: true,
-          coordinateSystem: "cartesian2d",
-          lineStyle: {
-            type: "line",
-            width: 2,
-            color: "#ccc",
-            curveness: 0.3,
-          },
-          effect: {
-            show: true,
-            trailLength: 0,
-            constantSpeed: 10,
-            symbol: "arrow",
-            color: "#ccc",
-            symbolSize: 6,
-          },
-          data: charts.linesData,
+        data: charts.nodes,
+      }, {
+        type: 'lines',
+        polyline: true,
+        coordinateSystem: 'cartesian2d',
+        lineStyle: {
+          type: 'line',
+          width: 2,
+          color: '#ccc',
+          curveness: 0.3
         },
-      ],
+        effect: {
+          show: true,
+          trailLength: 0,
+          constantSpeed: 10,
+          symbol: 'arrow',
+          color: '#ccc',
+          symbolSize: 6
+        },
+        data: charts.linesData,
+      }]
     };
     //@ts-ignore
     myChart.setOption(option);
