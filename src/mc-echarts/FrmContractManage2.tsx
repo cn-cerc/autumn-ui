9a2e98ae93cf3dd728de377e69c6a58e5ca694cc
郑振强 <13979669972@163.com>
2022-08-25 19:59:51 +0800

    add

--- src/mc-echarts/FrmContractManage2.tsx
+++ src/mc-echarts/FrmContractManage2.tsx
@@ -3,61 +3,77 @@ import * as echarts from "echarts";
 import React from "react";
 import FplApi from "../api/FplApi";
 import UIIntroduction from "../module/UIIntroduction";
-import styles from "./FrmContractManageMC1.css";
+import styles from "./FrmContractManage2.css";
 import { MCChartColors } from "./FrmTaurusMC";
 
-type FrmContractManageMC1TypeProps = {
-    dataJson: string,
-    introduction: string
+type FrmContractManage2TypeProps = {
+    
 }
 
-type FrmContractManageMC1TypeState = {
-    dataJson: DataRow,
+type FrmContractManage2TypeState = {
+    dataRow: DataRow,
     contractAmount: DataSet,
     contractTypeStats: DataSet,
     acceptedContract: DataSet,
+    introduction: string
 }
-//合同管理(庆丰物流)
+//合同管理(水泥厂)
 
-export default class FrmContractManageMC1 extends WebControl<FrmContractManageMC1TypeProps, FrmContractManageMC1TypeState> {
-    constructor(props: FrmContractManageMC1TypeProps) {
+export default class FrmContractManage2 extends WebControl<FrmContractManage2TypeProps, FrmContractManage2TypeState> {
+    constructor(props: FrmContractManage2TypeProps) {
         super(props);
         let lineRow = new DataRow();
-        let dataJson: DataRow = lineRow.setJson(this.props.dataJson);
+        let dataRow: DataRow = lineRow.setValue("银行维护_URL", "TFrmBankInfo")
+        .setValue("银行维护_Dis", true)
+        .setValue("合同类别_URL", "FrmContractType")
+        .setValue("合同类别_Dis", false)
+        .setValue("合同登记_URL", "FrmContract.selectContractType")
+        .setValue("合同登记_Dis", false)
+        .setValue("待接收合同_URL", "FrmContractApply")
+        .setValue("待接收合同_Dis", false)
+        .setValue("应付对账单_URL", "FrmTranCPBill")
+        .setValue("应付对账单_Dis", false)
+        .setValue("付款单_URL", "TFrmPaidAP")
+        .setValue("付款单_Dis", false)
+        .setValue("合同管理_URL", "FrmContract")
+        .setValue("合同管理_Dis", false);
+        let introduction = "主要用于所有合同的管理，这包括合同类别的管理、合同的管理、合同的登记、接收其他公司的合同，同时可以查看合同管理的数据统计以及数据分析";
+        
         this.state = {
-            dataJson: dataJson,
+            dataRow,
             contractAmount: new DataSet(),
             contractTypeStats: new DataSet(),
             acceptedContract: new DataSet(),
+            introduction
         }
     }
 
     render(): React.ReactNode {
         return <div className={styles.mc}>
-            <UIIntroduction introduction={this.props.introduction}></UIIntroduction>
+            <UIIntroduction introduction={this.state.introduction}></UIIntroduction>
             <div className={styles.mcMain}>
                 <div className={styles.mcFlowChartBox}>
                     <div className={styles.mcTitle}>流程图</div>
                     <div className={styles.mcFlowChartMain}>
                         <div className={styles.mcFlowChart}></div>
                         <div className={styles.mcFlowBox}>
-                            <div className={`${this.state.dataJson.getBoolean(`银行维护_Dis`) ? styles.other_disable : styles.other} ${styles.stock1}`} onClick={this.linkTo.bind(this, '银行维护')}>
+                            <div className={`${this.state.dataRow.getBoolean(`银行维护_Dis`) ? styles.other_disable : styles.other} ${styles.stock1}`} onClick={this.linkTo.bind(this, '银行维护')}>
                                 <span>银行维护</span>
                             </div>
-                            <div className={`${this.state.dataJson.getBoolean(`合同类别_Dis`) ? styles.receipt_disable : styles.receipt} ${styles.stock2}`} onClick={this.linkTo.bind(this, '合同类别')}>
+                            <div className={`${this.state.dataRow.getBoolean(`合同类别_Dis`) ? styles.receipt_disable : styles.receipt} ${styles.stock2}`} onClick={this.linkTo.bind(this, '合同类别')}>
                                 <span>合同类别</span>
                             </div>
-                            <div className={`${this.state.dataJson.getBoolean(`合同登记_Dis`) ? styles.register_disable : styles.register} ${styles.stock5}`} onClick={this.linkTo.bind(this, '合同登记')}>
+                            <div className={`${this.state.dataRow.getBoolean(`合同登记_Dis`) ? styles.register_disable : styles.register} ${styles.stock5}`} onClick={this.linkTo.bind(this, '合同登记')}>
                                 <span>合同登记</span>
                             </div>
-                            <div className={`${this.state.dataJson.getBoolean(`待接收合同_Dis`) ? styles.receipt_disable : styles.receipt} ${styles.stock7}`} onClick={this.linkTo.bind(this, '待接收合同')}>
+                            <div className={`${this.state.dataRow.getBoolean(`待接收合同_Dis`) ? styles.receipt_disable : styles.receipt} ${styles.stock7}`} onClick={this.linkTo.bind(this, '待接收合同')}>
                                 <span>待接收合同</span>
                             </div>
-                            <div className={`${this.state.dataJson.getBoolean(`合同管理_Dis`) ? styles.control_disable : styles.control} ${styles.stock8}`} onClick={this.linkTo.bind(this, '合同管理')}>
+                            <div className={`${this.state.dataRow.getBoolean(`合同管理_Dis`) ? styles.control_disable : styles.control} ${styles.stock8}`} onClick={this.linkTo.bind(this, '合同管理')}>
                                 <span>合同管理</span>
                             </div>
-                            <div className={`${this.state.dataJson.getBoolean(`应收对账单_Dis`) ? styles.receipt_disable : styles.receipt} ${styles.stock11}`} onClick={this.linkTo.bind(this, '应收对账单')}>
-                                <span>应收对账单</span>
+                            <div className={`${this.state.dataRow.getBoolean(`应付对账单_Dis`) ? styles.receipt_disable : styles.receipt} ${styles.stock11}`} onClick={this.linkTo.bind(this, '应付对账单')}>
+                                <span>应付对账单</span>
                             </div>
                         </div>
                     </div>
@@ -282,6 +298,7 @@ export default class FrmContractManageMC1 extends WebControl<FrmContractManageMC
         myChart.setOption(option);
     }
 
+
     initFlowChart() {
         let flowChart = document.querySelector(`.${styles.mcFlowChart}`) as HTMLDivElement;
         let myChart = echarts.init(flowChart);
@@ -385,8 +402,8 @@ export default class FrmContractManageMC1 extends WebControl<FrmContractManageMC
     }
 
     linkTo(name: string) {
-        if (!this.state.dataJson.getBoolean(`${name}_Dis`)) {
-            location.href = this.state.dataJson.getString(`${name}_URL`);
+        if (!this.state.dataRow.getBoolean(`${name}_Dis`)) {
+            location.href = this.state.dataRow.getString(`${name}_URL`);
         }
     }
 }
