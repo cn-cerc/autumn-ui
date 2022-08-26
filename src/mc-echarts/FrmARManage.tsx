1c195424ddad3874d57df161f68ca1fca8b535e4
lilu <956411079@qq.com>
2022-08-26 11:30:32 +0800

    同步4pl 应收/应付/费用/资金 MC至地藤

--- src/mc-echarts/FrmARManage.tsx
+++ src/mc-echarts/FrmARManage.tsx
@@ -1,24 +1,24 @@
 import { DataRow, DataSet, WebControl } from "autumn-ui";
 import React from "react";
-import styles from "./FrmARManageMC.css";
+import styles from "./FrmARManage.css";
 import * as echarts from "echarts";
 import { MCChartColors } from "./FrmTaurusMC";
 import UIIntroduction from "../module/UIIntroduction";
 
-type FrmARManageMCTypeProps = {
-    dataJson: string,
-    introduction: string
+type FrmARManageTypeProps = {
+    
 }
 
-type FrmARManageMCTypeState = {
+type FrmARManageTypeState = {
     lineData: DataSet,
     pieData1: DataSet
     pieData2: DataSet,
-    dataJson: DataRow,
+    dataRow: DataRow,
+    introduction: string
 }
 
-export default class FrmARManageMC extends WebControl<FrmARManageMCTypeProps, FrmARManageMCTypeState> {
-    constructor(props: FrmARManageMCTypeProps) {
+export default class FrmARManage extends WebControl<FrmARManageTypeProps, FrmARManageTypeState> {
+    constructor(props: FrmARManageTypeProps) {
         super(props);
         let lineData = new DataSet();
         let lineRow = new DataRow();
@@ -39,46 +39,79 @@ export default class FrmARManageMC extends WebControl<FrmARManageMCTypeProps, Fr
         pieData2.append().setValue('Value_', 20).setValue('Name_', '广西省');
         pieData2.append().setValue('Value_', 30).setValue('Name_', '湖南省');
         pieData2.append().setValue('Value_', 15).setValue('Name_', '广东省');
-        let dataJson: DataRow = lineRow.setJson(this.props.dataJson);
+        let dataRow: DataRow = lineRow.setValue("应收调整单_URL", "TFrmPaidRA")
+        .setValue("应收调整单_Dis", false)
+        .setValue("销售订单_URL", "TFrmTranOD")
+        .setValue("销售订单_Dis", false)
+        .setValue("物流运单_URL", "FrmArrangeCarExport")
+        .setValue("物流运单_Dis", false)
+        .setValue("应收对账作业_URL", "FrmARSourceToCR")
+        .setValue("应收对账作业_Dis", false)
+        .setValue("运单应收对账作业_URL", "FrmArrangeCarToCR")
+        .setValue("运单应收对账作业_Dis", false)
+        .setValue("应收对账单_URL", "FrmTranCRBill")
+        .setValue("应收对账单_Dis", false)
+        .setValue("收款(申请)单_URL", "TFrmPaidAR")
+        .setValue("收款(申请)单_Dis", false)
+        .setValue("应收账款_URL", "TFrmCheckAR")
+        .setValue("应收账款_Dis", false)
+        .setValue("收款单_URL", "TFrmPaidAR")
+        .setValue("收款单_Dis", false)
+        .setValue("会计凭证_URL", "TFrmAccBook")
+        .setValue("会计凭证_Dis", false)
+        .setValue("银行存款_URL", "TSchAccBook1300")
+        .setValue("银行存款_Dis", false);
+        let introduction = "主要用于公司主营业务中的所有关于应收的数据，同时连接应收调整单，应收结账单，以及应付账款，请款单，收款单，会计凭证，银行存款，结合起来即形成了完整的应收管理";
+        
         this.state = {
             lineData,
             pieData1,
             pieData2,
-            dataJson: dataJson,
+            dataRow,
+            introduction
         }
     }
 
     render(): React.ReactNode {
         return <div className={styles.mc}>
-            <UIIntroduction introduction={this.props.introduction}></UIIntroduction>
+            <UIIntroduction introduction={this.state.introduction}></UIIntroduction>
             <div className={styles.mcMain}>
                 <div className={styles.mcFlowChartBox}>
                     <div className={styles.mcTitle}>流程图</div>
                     <div className={styles.mcFlowChartMain}>
                         <div className={styles.mcFlowChart}></div>
                         <div className={styles.mcFlowBox}>
-                            <div className={`${this.state.dataJson.getBoolean(`应收调整单_Dis`) ? styles.receipt_disable : styles.receipt} ${styles.stock2}`} onClick={this.linkTo.bind(this, '应收调整单')}>
+                            <div className={`${this.state.dataRow.getBoolean(`应收调整单_Dis`) ? styles.receipt_disable : styles.receipt} ${styles.stock2}`} onClick={this.linkTo.bind(this, '应收调整单')}>
                                 <span>应收调整单</span>
                             </div>
-                            <div className={`${this.state.dataJson.getBoolean(`销售订单_Dis`) ? styles.receipt_disable : styles.receipt} ${styles.stock4}`} onClick={this.linkTo.bind(this, '销售订单')}>
+                            <div className={`${this.state.dataRow.getBoolean(`物流运单_Dis`) ? styles.receipt_disable : styles.receipt} ${styles.stock3}`} onClick={this.linkTo.bind(this, '物流运单')}>
+                                <span>物流运单</span>
+                            </div>
+                            <div className={`${this.state.dataRow.getBoolean(`销售订单_Dis`) ? styles.receipt_disable : styles.receipt} ${styles.stock4}`} onClick={this.linkTo.bind(this, '销售订单')}>
                                 <span>销售订单</span>
                             </div>
-                            <div className={`${this.state.dataJson.getBoolean(`应收对账单_Dis`) ? styles.receipt_disable : styles.receipt} ${styles.stock5}`} onClick={this.linkTo.bind(this, '应收对账单')}>
+                            <div className={`${this.state.dataRow.getBoolean(`应收对账单_Dis`) ? styles.receipt_disable : styles.receipt} ${styles.stock5}`} onClick={this.linkTo.bind(this, '应收对账单')}>
                                 <span>应收对账单</span>
                             </div>
-                            <div className={`${this.state.dataJson.getBoolean(`收款(申请)单_Dis`) ? styles.receipt_disable : styles.receipt} ${styles.stock7}`} onClick={this.linkTo.bind(this, '收款(申请)单')}>
+                            <div className={`${this.state.dataRow.getBoolean(`应收对账作业_Dis`) ? styles.receipt_disable : styles.receipt} ${styles.stock6}`} onClick={this.linkTo.bind(this, '应收对账作业')}>
+                                <span>应收对账作业</span>
+                            </div>
+                            <div className={`${this.state.dataRow.getBoolean(`运单应收对账作业_Dis`) ? styles.receipt_disable : styles.receipt} ${styles.stock9}`} onClick={this.linkTo.bind(this, '运单应收对账作业')}>
+                                <span>运单应收对账作业</span>
+                            </div>
+                            <div className={`${this.state.dataRow.getBoolean(`收款(申请)单_Dis`) ? styles.receipt_disable : styles.receipt} ${styles.stock7}`} onClick={this.linkTo.bind(this, '收款(申请)单')}>
                                 <span>收款(申请)单</span>
                             </div>
-                            <div className={`${this.state.dataJson.getBoolean(`应收账款_Dis`) ? styles.control_disable : styles.control} ${styles.stock8}`} onClick={this.linkTo.bind(this, '应收账款')}>
+                            <div className={`${this.state.dataRow.getBoolean(`应收账款_Dis`) ? styles.control_disable : styles.control} ${styles.stock8}`} onClick={this.linkTo.bind(this, '应收账款')}>
                                 <span>应收账款</span>
                             </div>
-                            <div className={`${this.state.dataJson.getBoolean(`收款单_Dis`) ? styles.receipt_disable : styles.receipt} ${styles.stock10}`} onClick={this.linkTo.bind(this, '收款单')}>
+                            <div className={`${this.state.dataRow.getBoolean(`收款单_Dis`) ? styles.receipt_disable : styles.receipt} ${styles.stock10}`} onClick={this.linkTo.bind(this, '收款单')}>
                                 <span>收款单</span>
                             </div>
-                            <div className={`${this.state.dataJson.getBoolean(`会计凭证_Dis`) ? styles.other_disable : styles.other} ${styles.stock12}`} onClick={this.linkTo.bind(this, '会计凭证')}>
+                            <div className={`${this.state.dataRow.getBoolean(`会计凭证_Dis`) ? styles.other_disable : styles.other} ${styles.stock12}`} onClick={this.linkTo.bind(this, '会计凭证')}>
                                 <span>会计凭证</span>
                             </div>
-                            <div className={`${this.state.dataJson.getBoolean(`银行存款_Dis`) ? styles.other_disable : styles.other} ${styles.stock13}`} onClick={this.linkTo.bind(this, '银行存款')}>
+                            <div className={`${this.state.dataRow.getBoolean(`银行存款_Dis`) ? styles.other_disable : styles.other} ${styles.stock13}`} onClick={this.linkTo.bind(this, '银行存款')}>
                                 <span>银行存款</span>
                             </div>
                         </div>
@@ -188,7 +221,6 @@ export default class FrmARManageMC extends WebControl<FrmARManageMCTypeProps, Fr
                 {
                     data: sData,
                     type: 'bar',
-                    name: '售出',
                     itemStyle: {
                         color: MCChartColors[0]
                     },
@@ -214,53 +246,81 @@ export default class FrmARManageMC extends WebControl<FrmARManageMCTypeProps, Fr
             nodes,
             linesData: [
                 {
-                    coords: [ //应收调整单 往下线条
-                        [168, 75],
-                        [168, 108]
+                    coords: [ 
+                        [170, 65],
+                        [170, 96]
                     ]
                 },
                 {
-                    coords: [ //销货单 往右线条
-                        [73, 123],
-                        [150, 123],
+                    coords: [ 
+                        [50, 65],
+                        [50, 80],
+                        [170, 80]
                     ]
                 },
                 {
-                    coords: [ //应收对账单 往右下线条
-                        [190, 123],
-                        [279, 123],
-                        [279, 271]
+                    coords: [ 
+                        [154, 113],
+                        [60, 113]
                     ]
                 },
                 {
-                    coords: [ //应收对账单 往下线条
-                        [168, 160],
-                        [168, 189]
+                    coords: [ 
+                        [182, 113],
+                        [256, 113]
                     ]
                 },
                 {
-                    coords: [ //应收对账单 往左下线条
-                        [168, 174],
-                        [50, 174],
-                        [50, 189]
+                    coords: [ 
+                        [50, 146],
+                        [50, 156],
+                        [170, 156],
+                        [170, 175]
                     ]
                 },
                 {
-                    coords: [ //应收账款 往下线条
-                        [50, 247],
-                        [50, 271]
+                    coords: [ 
+                        [278, 146],
+                        [278, 156],
+                        [170, 156]
                     ]
                 },
                 {
-                    coords: [ //收款单 往右线条
-                        [73, 290],
-                        [256, 290]
+                    coords: [ 
+                        [170, 222],
+                        [170, 254]
                     ]
                 },
                 {
-                    coords: [ //收款单 往下线条
-                        [50, 328],
-                        [50, 349]
+                    coords: [ 
+                        [170, 235],
+                        [50, 235],
+                        [50, 254]
+                    ]
+                },
+                {
+                    coords: [ 
+                        [50, 308],
+                        [50, 336]
+                    ]
+                },
+                {
+                    coords: [ 
+                        [50, 386],
+                        [50, 416]
+                    ]
+                },
+                {
+                    coords: [ 
+                        [60, 352],
+                        [265, 352]
+                    ]
+                },
+                {
+                    coords: [ 
+                        [182, 186],
+                        [278, 186],
+                        [278, 336]
                     ]
                 },
             ]
@@ -328,8 +388,8 @@ export default class FrmARManageMC extends WebControl<FrmARManageMCTypeProps, Fr
     }
 
     linkTo(name: string) {
-        if (!this.state.dataJson.getBoolean(`${name}_Dis`)) {
-            location.href = this.state.dataJson.getString(`${name}_URL`);
+        if (!this.state.dataRow.getBoolean(`${name}_Dis`)) {
+            location.href = this.state.dataRow.getString(`${name}_URL`);
         }
     }
 
